b4fa10271ca2a186be97c86b147e3dc0ae028c34
cau0730 <cau0730@users.sourceforge.net>
2002-07-03 06:56:50 +0000

    Formatted the code better and also added discriptions for each function

--- system/apps/utils/desktop/properties.h
+++ system/apps/utils/desktop/properties.h
@@ -16,84 +16,84 @@ typedef vector <std::string> t_List;
 class MiscView : public View
 {
 
-public:
-	MiscView(const Rect & cFrame);
-	CheckBox* pcLoginCheck;
-	CheckBox* pcShowVerCheck;
+    public:
+        MiscView(const Rect & cFrame);
+        CheckBox* pcLoginCheck;
+        CheckBox* pcShowVerCheck;
 };
 
 
 class BackView : public View
 {
-public:
-	BackView(const Rect & cFrame);
-	ListView* pcList;
-	DropdownMenu* pcSizeDrop;
-	t_List ImageList();
-	Bitmap* pcScreenBmp;
-    virtual void Paint(const Rect& cUpdate);
-    void ListFiles();
-  
-    
-	
-private:
-	
-	t_ListRows vFolderRow;
-	StringView* pcString;
-	void Defaults();
-	
+    public:
+        BackView(const Rect & cFrame);
+        ListView* pcList;
+        DropdownMenu* pcSizeDrop;
+        t_List ImageList();
+        Bitmap* pcScreenBmp;
+        virtual void Paint(const Rect& cUpdate);
+        void ListFiles();
+
+
+
+    private:
+
+        t_ListRows vFolderRow;
+        StringView* pcString;
+        void Defaults();
+
 };
 
 
 
 class ColorView : public View
 {
-public:
-	ColorView(const Rect& cFrame);
-	DropdownMenu* pcColorDrop;
-	ListView* pcListTheme;
-	ColorEdit* pcColorEdit;
-	virtual void Paint(const Rect& cUpdate);
-	Bitmap* pcScreenBmp;
+    public:
+        ColorView(const Rect& cFrame);
+        DropdownMenu* pcColorDrop;
+        ListView* pcListTheme;
+        ColorEdit* pcColorEdit;
+        virtual void Paint(const Rect& cUpdate);
+        Bitmap* pcScreenBmp;
 };
 
 
 class PropTab : public TabView
 {
-public:
-	PropTab(const Rect & cFrame);
-	 ColorView* pcColor;
-	 MiscView* pcMisc;
-	 BackView* pcBack;
-private:
-	void Defaults();
-	
+    public:
+        PropTab(const Rect & cFrame);
+        ColorView* pcColor;
+        MiscView* pcMisc;
+        BackView* pcBack;
+    private:
+        void Defaults();
+
 };
 
 
 class PropWin : public Window
 {
-public:
-	PropWin();
-    PropTab* pcPropTab;
+    public:
+        PropWin();
+        PropTab* pcPropTab;
 
-private:
-	Button* pcCancel;
-	Button* pcSave;
-	Button* pcClose;
-	void HandleMessage(Message* pcMessage);
-	void Defaults();
-	void ReadLoginConfig();
-	char zLoginName[1024];
-	void Save();
-	void SaveLoginConfig(bool b_Login, const char* zName);
-	bool OkToQuit();
-	void LoadPrefs();
-	void SavePrefs(bool bShow, bool bLogin, string zPic, int32 nNewImageSize);
-	
-	bool bShwVr;
-	const char* dImage;
-    string zImage;
+    private:
+        Button* pcCancel;
+        Button* pcSave;
+        Button* pcClose;
+        void HandleMessage(Message* pcMessage);
+        void Defaults();
+        void ReadLoginConfig();
+        char zLoginName[1024];
+        void Save();
+        void SaveLoginConfig(bool b_Login, const char* zName);
+        bool OkToQuit();
+        void LoadPrefs();
+        void SavePrefs(bool bShow, bool bLogin, string zPic, int32 nNewImageSize);
+
+        bool bShwVr;
+        const char* dImage;
+        string zImage;
 };
 
 #endif
