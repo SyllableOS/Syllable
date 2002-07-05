76298109d55a10df350e47975e457f6fba0aded1
cau0730 <cau0730@users.sourceforge.net>
2002-07-05 16:47:10 +0000

    *** empty log message ***

---
+++ system/apps/utils/aview/main.cpp
@@ -0,0 +1,37 @@
+//  AView 0.2 -:-  (C)opyright 2000-2001 Kristian Van Der Vliet
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Library General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Library General Public License for more details.
+//
+//  You should have received a copy of the GNU Library General Public
+//  License along with this library; if not, write to the Free
+//  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+//  MA 02111-1307, USA
+//
+
+//Local include files
+
+#include "mainapp.h"
+
+//Main
+
+int main(void);
+
+int main(void){
+
+  ImageApp* pc_ImageApp = new ImageApp();
+  pc_ImageApp->Run();
+  
+  return(0);
+
+}
+
+
+
---
+++ system/apps/utils/aview/mainapp.h
@@ -0,0 +1,54 @@
+//  AView 0.2 -:-  (C)opyright 2000-2001 Kristian Van Der Vliet
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Library General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Library General Public License for more details.
+//
+//  You should have received a copy of the GNU Library General Public
+//  License along with this library; if not, write to the Free
+//  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+//  MA 02111-1307, USA
+//
+
+#include <util/application.h>
+#include "window.h"
+
+using namespace os;
+
+class ImageApp : public Application
+{
+ public:
+   ImageApp();
+   ~ImageApp();
+   
+ private:
+   AppWindow* m_pcMainWindow;
+   
+};
+
+ImageApp::ImageApp() : Application( "application/x-VND.RGC-AView")
+{
+   m_pcMainWindow = new AppWindow(Rect(100,125,400,400));
+
+   m_pcMainWindow->AddItems(m_pcMainWindow);
+   
+   m_pcMainWindow->Show();
+   m_pcMainWindow->MakeFocus();
+
+}
+
+ImageApp::~ImageApp()
+{
+   
+}
+
+
+
+
+
