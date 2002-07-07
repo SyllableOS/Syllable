a39a116f40637ad16257db3a388cc9a572a04a04
cau0730 <cau0730@users.sourceforge.net>
2002-07-07 17:01:33 +0000

    Initial version of removeattrib from Henrik :).  Thanks, Henrik!

---
+++ system/apps/utils/fsutils/rmattrib.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <errno.h>
+#include <dirent.h>
+
+#include <atheos/fs_attribs.h>
+
+void print_usage( const char* pzName )
+{
+	printf( "usage: %s path attr_name\n", pzName );
+}
+
+int main( int argc, char** argv )
+{
+	int nFile;
+	int i;
+	DIR* pDir;
+	struct dirent* psEntry;
+  
+	if ( argc < 3 ) {
+		print_usage( argv[0] );
+		return( 1 );
+	}
+
+	nFile = open( argv[1], O_RDWR );
+
+	if ( nFile < 0 ) {
+		printf( "Failed to open %s: %s\n", argv[1], strerror( errno ) );
+		return( 1 );
+	}
+  
+	remove_attr( nFile, argv[2] );
+  
+	close( nFile );
+
+	return( 0 );
+}
+
+
+
+
