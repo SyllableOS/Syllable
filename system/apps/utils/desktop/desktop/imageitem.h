2f1093f31e1f5247733c803039bb9d4823d6068d
cau0730 <cau0730@users.sourceforge.net>
2002-08-16 05:24:10 +0000

    Moved to Hendrik's newest imageitem code, and I also added bitmaps for menus and the submenu arrow from the menu class

--- system/apps/utils/desktop/desktop/imageitem.h
+++ system/apps/utils/desktop/desktop/imageitem.h
@@ -4,41 +4,51 @@
 #include <gui/menu.h>
 #include <gui/bitmap.h>
 #include <gui/font.h>
+
+#define SUB_MENU_ARROW_W 10
+#define SUB_MENU_ARROW_H 10
+
+
+
+
 using namespace os;
 
 class ImageItem : public MenuItem
 {
-    public:
-        ImageItem( const char* pzLabel, Message* pcMsg, const char *shortcut = NULL, Bitmap *bmap = NULL);
-        ImageItem( Menu* pcMenu, Message* pcMsg, Bitmap* bmap );
-        ~ImageItem();
+	public:
+	ImageItem( const char* pzLabel, Message* pcMsg, const char *shortcut = NULL, Bitmap *bmap = NULL);
+	ImageItem( Menu* pcMenu, Message* pcMsg = NULL, Bitmap *bmap = NULL);
+	~ImageItem();
 
-        virtual void  Draw();
-        virtual void  DrawContent();
-        virtual void  Highlight( bool bHighlight );
-        virtual Point GetContentSize();
+	virtual void  Draw();
+	virtual void  DrawContent();
+	virtual void  Highlight( bool bHighlight );
+	virtual Point GetContentSize();
 
-        void SetBitmap(Bitmap *bm);
-        Bitmap *GetBitmap() { return m_Bitmap; }
+	void SetBitmap(Bitmap *bm);
+	Bitmap *GetBitmap() { return m_Bitmap; }
 
-        void SetShortcut(const char *shortcut);
-        const char *GetShortcut() { return m_Shortcut; }
+	void SetShortcut(const char *shortcut);
+	const char *GetShortcut() { return m_Shortcut; }
 
-    private:
+	private:
 
-        void Init();
+	void Init();
 
-        bool	m_Enabled;
-        bool	m_Highlighted;
-        bool	m_HasIcon;
-        int	m_IconWidth;
-        int	m_IconHeight;
-        char	*m_Shortcut;
-        Bitmap	*m_Bitmap;
+	bool	m_Enabled;
+	bool	m_Highlighted;
+	bool	m_HasIcon;
+	int	m_IconWidth;
+	int	m_IconHeight;
+	char	*m_Shortcut;
+	Bitmap	*m_Bitmap;
 };
 
 
 #endif
+
+
+
 
 
 
