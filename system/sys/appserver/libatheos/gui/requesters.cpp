62a3a3bd6707a88e00368ed5aa78fb62aa71222c
vanders <vanders@liqwyd.com>
2002-08-25 13:30:10 +0000

    Can now display an image on Alerts

--- system/sys/appserver/libatheos/gui/requesters.cpp
+++ system/sys/appserver/libatheos/gui/requesters.cpp
@@ -16,7 +16,6 @@
  *  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  *  MA 02111-1307, USA
  */
-
 #include <gui/requesters.h>
 #include <gui/desktop.h>
 #include <gui/font.h>
@@ -24,24 +23,82 @@
 
 using namespace os;
 
-//----------------------------------------------------------------------------
-// NAME:
-// DESC:
-// NOTE:
-// SEE ALSO:
-//----------------------------------------------------------------------------
 
+/** Initialize the Alert.
+ * \par Description:
+ *	The Alert is like a Messagebox in windows.  It provides a way to show errors,
+ *  About boxes or even tips without having to create your own Window. 
+ * \param cTitle - The title of the window.
+ * \param cText  - The text that will be shown in the Alert.
+   \param nFlags - The flags that will be passed to the appserver to tell the appserver what options
+   				   when creating this.
+ * \param ...    - The name of buttons to create(IE: would be like "ok" or "cancel").  You can create 
+ 					more than one button(IE: new Alert("Title","Text",WND_NOT_RESIZABLE,"OK", "CANCEL");).  
+ * \sa Go() 
+ * \author	Kurt Skauen (kurt@atheos.cx) with modifications by Rick Caudill
+ *****************************************************************************/
 Alert::Alert( const std::string& cTitle, const std::string& cText, int nFlags, ... ) :
-    Window( Rect( 100, 50, 100, 50 ), "alert_window", cTitle.c_str() )
+    Window( Rect( 100, 50, 100, 50 ), "alert_window", cTitle.c_str(), WND_NOT_RESIZABLE | WND_NO_ZOOM_BUT | WND_NO_DEPTH_BUT)
+{
+    
+    va_list pArgs;
+	va_start( pArgs, nFlags );
+	cm_pcBitmap = NULL;
+    
+    m_pcView = new AlertView( cText, pArgs );
+    m_pcView->SetFgColor( 0, 0, 0 );
+    m_pcView->SetBgColor( get_default_color(COL_NORMAL) );
+    Point cSize = m_pcView->GetPreferredSize( true );
+    m_pcView->ResizeTo( cSize );
+    ResizeTo( cSize );
+    
+    if (nFlags > 0){
+    	SetFlags(nFlags);
+    	}
+    Desktop cDesktop;
+    MoveTo( cDesktop.GetResolution().x / 2 - cSize.x / 2, cDesktop.GetResolution().y / 2 - cSize.y / 2 );
+  
+    AddChild( m_pcView );
+    va_end( pArgs );
+    m_pcInvoker = NULL;
+    m_hMsgPort  = -1;
+    
+    Flush();
+    Sync();
+}
+
+/** Initialize the Alert.
+ * \par Description:
+ *	The Alert is like a Messagebox in windows.  It provides a way to show errors,
+ *  About boxes or even tips without having to create your own Window. 
+ * \param cTitle   - The title of the window.
+ * \param cText    - The text that will be shown in the Alert.
+ * \param nFlags   - The flags that will be passed to the appserver to tell the appserver what options
+   				     when creating this.
+ * \param pcBitmap - Bitmap that will show up in the Alert Window. 
+ * \param ...      - The name of buttons to create(IE: would be like "ok" or "cancel").  You can create 
+ 					more than one button(IE: new Alert("Title","Text",WND_NOT_RESIZABLE,"OK", "CANCEL");).  
+ * \sa Go() 
+ * \author	Kurt Skauen (kurt@atheos.cx) with modifications by Rick Caudill
+ *****************************************************************************/
+Alert::Alert( const std::string& cTitle, const std::string& cText, Bitmap* pcBitmap, int nFlags, ... ) :
+    Window( Rect( 100, 50, 100, 50 ), "alert_window", cTitle.c_str(), WND_NOT_RESIZABLE | WND_NO_ZOOM_BUT | WND_NO_DEPTH_BUT)
 {
     va_list pArgs;
 
     va_start( pArgs, nFlags );
-
-    m_pcView = new AlertView( cText, pArgs );
+    
+     if (nFlags > 0){
+    	SetFlags(nFlags);
+    	}
+    	
+    cm_pcBitmap = NULL;	
+    m_pcView = new AlertView( cText, pArgs, pcBitmap );
+    
+    m_pcView->SetEraseColor(get_default_color(COL_NORMAL));
     m_pcView->SetFgColor( 0, 0, 0 );
-    m_pcView->SetBgColor( 200, 200, 200 );
-    Point cSize = m_pcView->GetPreferredSize( false );
+    m_pcView->SetBgColor( get_default_color(COL_NORMAL) );
+    Point cSize = m_pcView->GetPreferredSize( true );
     m_pcView->ResizeTo( cSize );
     ResizeTo( cSize );
 
@@ -52,6 +109,70 @@ Alert::Alert( const std::string& cTitle, const std::string& cText, int nFlags, .
     va_end( pArgs );
     m_pcInvoker = NULL;
     m_hMsgPort  = -1;
+   
+   	m_pcView->Paint(GetBounds());
+   	
+   	Flush();
+    Sync();
+}
+
+
+/** Initialize the Alert.
+ * \par Description:
+ *	The Alert is like a Messagebox in windows.  It provides a way to show errors,
+ *  About boxes or even tips without having to create your own Window. 
+ * \param cTitle    - The title of the window.
+ * \param cText     - The text that will be shown in the Alert.
+ * \param nFlags    - The flags that will be passed to the appserver to tell the appserver what options
+   				       when creating this.
+ * \param nAlertNum - Static icons that will show up when you call this constructor. To call the icons you would do something like this: (new Alert("Title","Text", Alert::ALERT_WARNING,0."OK",NULL);)
+ * \param ...       - The name of buttons to create(IE: would be like "ok" or "cancel").  You can create 
+ 					more than one button(IE: new Alert("Title","Text",WND_NOT_RESIZABLE,"OK", "CANCEL");).  
+ * \sa Go() 
+ * \author	Kurt Skauen (kurt@atheos.cx) with modifications by Rick Caudill
+ *****************************************************************************/
+Alert::Alert( const std::string& cTitle, const std::string& cText, alert_icon nAlertNum, int nFlags, ... ) :
+    Window( Rect( 100, 50, 100, 50 ), "alert_window", cTitle.c_str(), WND_NOT_RESIZABLE | WND_NO_ZOOM_BUT | WND_NO_DEPTH_BUT )
+{
+    va_list pArgs;
+
+    va_start( pArgs, nFlags );
+    
+    nImage = nAlertNum;
+    
+     if (nFlags > 0){
+    	SetFlags(nFlags);
+    	}
+    
+    cm_pcBitmap = 0;
+    	
+    switch (nImage){
+    	
+     case ALERT_WARNING:  SetImage(nWarning_Icon.width, nWarning_Icon.height, (uint8 *) nWarning_Icon.pixel_data); break;
+	 case ALERT_INFO:     SetImage(nInfo_Icon.width, nInfo_Icon.height, (uint8 *) nInfo_Icon.pixel_data); break;
+     case ALERT_QUESTION: SetImage(nQuestion_Icon.width, nQuestion_Icon.height, (uint8 *) nQuestion_Icon.pixel_data); break;
+     default: break;	
+    }
+    m_pcView = new AlertView( cText, pArgs, cm_pcBitmap );
+    	
+    m_pcView->SetEraseColor(get_default_color(COL_NORMAL));
+    m_pcView->SetFgColor( 0, 0, 0 );
+    m_pcView->SetBgColor( get_default_color(COL_NORMAL) );
+    Point cSize = m_pcView->GetPreferredSize( true );
+    m_pcView->ResizeTo( cSize );
+    ResizeTo( cSize );
+
+    Desktop cDesktop;
+    MoveTo( cDesktop.GetResolution().x / 2 - cSize.x / 2, cDesktop.GetResolution().y / 2 - cSize.y / 2 );
+  
+    AddChild( m_pcView );
+    va_end( pArgs );
+    m_pcInvoker = NULL;
+    m_hMsgPort  = -1;
+    m_pcView->Paint(GetBounds());
+    
+    Flush();
+    Sync();
 }
 
 //----------------------------------------------------------------------------
@@ -67,6 +188,10 @@ Alert::~Alert()
 	delete_port( m_hMsgPort );
     }
     delete m_pcInvoker;
+    
+    if (cm_pcBitmap != NULL){
+   	delete cm_pcBitmap;
+	}
 }
 
 //----------------------------------------------------------------------------
@@ -129,11 +254,64 @@ int Alert::Go()
     }
 }
 
+
+
+//----------------------------------------------------------------------------
+// NAME:
+// DESC:
+// NOTE:
+// SEE ALSO:
+//----------------------------------------------------------------------------
+
+
 void Alert::Go( Invoker* pcInvoker )
 {
     m_pcInvoker = pcInvoker;
     Show();
     MakeFocus();
+}
+
+
+
+//----------------------------------------------------------------------------
+// NAME: SetImage()
+// DESC: SetImage() is a private method for adding alert icons to the alert
+// NOTE:
+// SEE ALSO:
+//----------------------------------------------------------------------------
+
+void Alert::SetImage( uint32 nWidth, uint32 nHeight, uint8 *pnBuffer )
+{
+	
+	if( cm_pcBitmap != 0 )
+		delete cm_pcBitmap;
+		
+	uint32 nBufferSize = nWidth * nHeight * 4;
+		
+	cm_pcBitmap = new Bitmap( nWidth, nHeight, CS_RGB32 );
+	
+	Color32_s cBGCol = get_default_color( COL_NORMAL );
+	
+	uint8 *pnRaster = cm_pcBitmap->LockRaster( );
+	
+	for( uint32 nByte = 0; nByte < nBufferSize; nByte+=4 ) {
+		if( (pnBuffer[nByte] != 0) || (pnBuffer[nByte + 1] != 0) || (pnBuffer[nByte + 2] != 0) ) {
+			(*pnRaster++) = pnBuffer[nByte+2];
+			(*pnRaster++) = pnBuffer[nByte+1];
+			(*pnRaster++) = pnBuffer[nByte];
+			(*pnRaster++) = pnBuffer[nByte+3];
+		} else {
+			(*pnRaster++) = cBGCol.blue;
+			(*pnRaster++) = cBGCol.green;
+			(*pnRaster++) = cBGCol.red;
+			(*pnRaster++) = cBGCol.alpha;
+		}
+	
+	}
+	
+	cm_pcBitmap->UnlockRaster( );
+
+	Flush();	
 }
 
 //----------------------------------------------------------------------------
@@ -143,10 +321,10 @@ void Alert::Go( Invoker* pcInvoker )
 // SEE ALSO:
 //----------------------------------------------------------------------------
 
-AlertView::AlertView( const std::string& cText,va_list pButtons ) : View( Rect(0,0,1,1), "alert", CF_FOLLOW_ALL )
+AlertView::AlertView( const std::string& cText,va_list pButtons, Bitmap* pcBitmap ) : View( Rect(0,0,1,1), "alert", CF_FOLLOW_ALL )
 {
     const char* pzButName;
-
+	pcBitmp = pcBitmap;
     uint i = 0;
 
     while( (pzButName = va_arg( pButtons, const char* )) != NULL )
@@ -155,7 +333,7 @@ AlertView::AlertView( const std::string& cText,va_list pButtons ) : View( Rect(0
 	m_cButtons.push_back( pcButton );
 	AddChild( pcButton );
 
-	Point cSize = pcButton->GetPreferredSize( false );
+	Point cSize = pcButton->GetPreferredSize( true );
 	if ( cSize.x > m_cButtonSize.x ) {
 	    m_cButtonSize.x = cSize.x;
 	}
@@ -181,6 +359,7 @@ AlertView::AlertView( const std::string& cText,va_list pButtons ) : View( Rect(0
 	}
     }
 
+    float x;
     font_height sFontHeight;
     GetFontHeight( &sFontHeight );
     m_vLineHeight = sFontHeight.ascender + sFontHeight.descender + sFontHeight.line_gap;
@@ -189,14 +368,43 @@ AlertView::AlertView( const std::string& cText,va_list pButtons ) : View( Rect(0
     nHeight += m_cButtonSize.y + 30;
 
     float y = nHeight - m_cButtonSize.y - 5;
-    float x = nWidth - m_cButtons.size() * (m_cButtonSize.x + 5) - 5;
-  
+    
+    if(pcBitmap != NULL){
+    		x = nWidth - m_cButtons.size() * (m_cButtonSize.x + 5) + pcBitmap->GetBounds().Width();
+    	}
+    	
+    	else{
+    		x = nWidth - m_cButtons.size() * (m_cButtonSize.x + 5) - 5; // sets button placement
+  }
     for ( i = 0 ; i < m_cButtons.size() ; ++i ) {
-	m_cButtons[i]->SetFrame( Rect( x, y, x + m_cButtonSize.x - 1, y + m_cButtonSize.y - 1 ) );
-	x += m_cButtonSize.x + 5;
+    	if(pcBitmap != NULL){
+			
+			m_cButtons[i]->SetFrame( Rect( x, y, x + m_cButtonSize.x, y + m_cButtonSize.y - 1 ) ); //look here for button position manip
+			x += m_cButtonSize.x + 5;
+			m_cButtons[i]->Flush();
+			m_cButtons[i]->Sync();
+			
+			} else {
+		
+    		m_cButtons[i]->SetFrame( Rect( x, y, x + m_cButtonSize.x + 5, y + m_cButtonSize.y - 1 ) ); //look here for button position manip
+			x += m_cButtonSize.x + 5; // was 5
+			m_cButtons[i]->Flush();
+			m_cButtons[i]->Sync();
+			
+			}
+    
     }
-    m_cMinSize.x = nWidth;
-    m_cMinSize.y = nHeight;
+    
+    if (pcBitmp !=NULL){
+    	m_cMinSize.x = nWidth + pcBitmp->GetBounds().Width() + 5 ;
+    	 
+   	}	else{
+   		m_cMinSize.x = nWidth;  //was 0
+    
+    }m_cMinSize.y = nHeight + 4 ;
+
+	Invalidate();
+	
 }
 
 void AlertView::AllAttached()
@@ -225,16 +433,43 @@ Point AlertView::GetPreferredSize( bool bLargest )
 
 void AlertView::Paint( const Rect& cUpdateRect )
 {
-    FillRect( cUpdateRect, GetBgColor() );
-  
-    float y = 20.0f;
-    for ( uint i = 0 ; i < m_cLines.size() ; ++i )
-    {
-	MovePenTo( 10, y );
+    FillRect( GetBounds(), get_default_color(COL_NORMAL) );
+    
+	if (pcBitmp !=NULL){
+	
+	SetDrawingMode(DM_BLEND);
+	DrawBitmap(pcBitmp,pcBitmp->GetBounds(),Rect(5,5,pcBitmp->GetBounds().Width() + 1,pcBitmp->GetBounds().Height()));
+	float y = 20.0f;
+    
+    for ( uint i = 0 ; i < m_cLines.size() ; ++i ){
+   
+	MovePenTo( 20 + pcBitmp->GetBounds().Width(), y  );
 	DrawString( m_cLines[i].c_str() );
 	y += m_vLineHeight;
     }
+    
+    }
+    
+    else{
+	float y = 20.0f;
+    for ( uint i = 0 ; i < m_cLines.size() ; ++i )
+    {
+	if(pcBitmp !=NULL){
+		MovePenTo( pcBitmp->GetBounds().Width()/2 + 2, pcBitmp->GetBounds().Height()/2 + 7);
+		
+	} else {
+		MovePenTo(10,y);
+		}	
+	DrawString( m_cLines[i].c_str() );
+	y += m_vLineHeight;
+    }
+    }
+    
+    //Invalidate();
+   
+    
 }
+
 
 //----------------------------------------------------------------------------
 // NAME:
@@ -445,3 +680,50 @@ void ProgressView::Layout( const Rect& cBounds )
     }
     Flush();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
