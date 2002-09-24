508bdd8373a4224743772876b2683d06a9df770d
vanders <vanders@liqwyd.com>
2002-09-24 17:28:07 +0000

    Displays if a port is public or private

--- system/apps/utils/systools/ls_ports.cpp
+++ system/apps/utils/systools/ls_ports.cpp
@@ -9,8 +9,8 @@ int main()
 
     get_port_info( -1, &sInfo );
     int nError = 1;
-    printf( "PortID Owner  Max Cur Name\n" );
+    printf( "PortID Owner  Max Cur Priv/Pub  Name\n" );
     for (  ; nError == 1 ; nError = get_next_port_info( &sInfo ) ) {
-	printf( "%-06d %-06d %-03d %-03d %s\n", sInfo.pi_port_id, sInfo.pi_owner, sInfo.pi_max_count, sInfo.pi_cur_count, sInfo.pi_name );
+	printf( "%-06d %-06d %-03d %-03d %s\t%s\n", sInfo.pi_port_id, sInfo.pi_owner, sInfo.pi_max_count, sInfo.pi_cur_count, (sInfo.pi_flags ==  MSG_PORT_PRIVATE) ? "Private" : "Public" , sInfo.pi_name);
     }
 }
