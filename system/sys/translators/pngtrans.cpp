5e18884c9a9eb8c7e5502baeba429c3d64c8fc99
digitaliz <digitaliz@users.sourceforge.net>
2002-09-27 16:45:10 +0000

    Added save support

--- system/sys/translators/pngtrans.cpp
+++ system/sys/translators/pngtrans.cpp
@@ -2,7 +2,7 @@
 #include <setjmp.h>
 
 #include <atheos/kdebug.h>
-
+#include <gui/bitmap.h>		// BitsPerPixel()
 #include <util/circularbuffer.h>
 #include <translation/translator.h>
 
@@ -10,15 +10,12 @@
 #include <new>
 #include <png.h>
 
-
 using namespace os;
-
-
 
 class PNGTrans : public Translator
 {
 public:
-    PNGTrans();
+    PNGTrans( bool bWriteMode = false );
     virtual ~PNGTrans();
 
     virtual void     SetConfig( const Message& cConfig ) {}
@@ -32,29 +29,50 @@ public:
     int InfoCallback();
     int RowCallback( int nPass, int y, png_bytep pRow );
     int EndCallback();
+    void WriteCallback( png_bytep pData, int nSize );
     
 private:
     void AddProcessedData( void* pData, int nLen ) {
+    	static int total = 0;
 	m_cOutBuffer.Write( pData, nLen );
     }
-    BitmapHeader       m_sBitmapHeader;
-    BitmapFrameHeader  m_sCurrentFrame;
-    CircularBuffer     m_cOutBuffer;
+    void AllocRowBuffer( int nPixels ) {
+	if( m_pRowBuffer) delete m_pRowBuffer;
+    	m_pRowBuffer = new png_byte[nPixels * 3];
+    	m_nRowSize = nPixels * 3;
+    }
+    bool AppendToRowBuffer( void );
+
+    BitmapHeader	m_sBitmapHeader;
+    BitmapFrameHeader	m_sCurrentFrame;
+    CircularBuffer  	m_cOutBuffer;
+    CircularBuffer  	m_cInBuffer;		// Only for writing
     
-    int		       m_nNumPasses;
-    bool	       m_bIsInterlaced;
+    int		   	m_nNumPasses;
+    bool		m_bIsInterlaced;
     enum {
         STATE_INIT,
         STATE_START_DECOMPRESS,
         STATE_DECOMPRESSING,
         STATE_DO_OUTPUT_SCAN,
+        STATE_FRAMEHDR,
+        STATE_READING
     } m_eState;
 
-    png_structp    m_psPNGStruct;
-    png_infop	   m_psPNGInfo;
+    png_structp    	m_psPNGStruct;
+    png_infop	   	m_psPNGInfo;
+    png_byte*		m_pRowBuffer;
+    int			m_nRowPos;
+    int			m_nRowSize;
+    
+    bool		m_bWriteMode;
 };
 
-
+static void write_callback( png_structp png_ptr, png_bytep data, png_size_t size )
+{
+    PNGTrans* psTrans = (PNGTrans*)png_get_io_ptr( png_ptr );
+    psTrans->WriteCallback( data, size );
+}
 
 static void info_callback( png_structp png_ptr, png_infop info )
 {
@@ -112,48 +130,81 @@ static void end_callback(png_structp png_ptr, png_infop info)
 
 
 
-PNGTrans::PNGTrans()
+PNGTrans::PNGTrans( bool bWriteMode = false )
 {
-    m_eState		= STATE_INIT;
-    m_bIsInterlaced     = false;
-    
-    m_psPNGStruct = png_create_read_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+	m_eState		= STATE_INIT;
+	m_bIsInterlaced		= false;
+	m_bWriteMode		= bWriteMode;
+	m_pRowBuffer		= NULL;
+	m_nRowPos		= 0;
 
-   if ( m_psPNGStruct == NULL ) {
-       throw std::bad_alloc();
-   }
+	//dbprintf( "PNG: WriteMode = %d\n", bWriteMode );
 
-   m_psPNGInfo = png_create_info_struct( m_psPNGStruct );
+	if( bWriteMode ) {
+		m_psPNGStruct = png_create_write_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+		if ( m_psPNGStruct == NULL ) {
+			throw std::bad_alloc();
+		}
 
-   if ( m_psPNGInfo == NULL )
-   {
-      png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL);
-      throw std::bad_alloc();
-   }
+		m_psPNGInfo = png_create_info_struct( m_psPNGStruct );
 
-   if ( setjmp( m_psPNGStruct->jmpbuf ) ) {
-      png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL );
-      throw std::bad_alloc();
-   }
+		if ( m_psPNGInfo == NULL )
+		{
+			png_destroy_write_struct( &m_psPNGStruct, (png_infopp)NULL);
+			throw std::bad_alloc();
+		}
+
+		if ( setjmp( m_psPNGStruct->jmpbuf ) ) {
+			png_destroy_write_struct( &m_psPNGStruct, (png_infopp)NULL);
+			throw std::bad_alloc();
+		}
+
+		png_set_write_fn( m_psPNGStruct, (void *)this, write_callback, NULL);
+		
+		//dbprintf( "PNG: Write mode initialized\n" );
+	} else {
+		m_psPNGStruct = png_create_read_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+		if ( m_psPNGStruct == NULL ) {
+			throw std::bad_alloc();
+		}
+
+		m_psPNGInfo = png_create_info_struct( m_psPNGStruct );
+
+		if ( m_psPNGInfo == NULL )
+		{
+			png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL);
+			throw std::bad_alloc();
+		}
+
+		if ( setjmp( m_psPNGStruct->jmpbuf ) ) {
+			png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL );
+			throw std::bad_alloc();
+		}
    
-   /* This one's new.  You will need to provide all three
-    * function callbacks, even if you aren't using them all.
-    * If you aren't using all functions, you can specify NULL
-    * parameters.  Even when all three functions are NULL,
-    * you need to call png_set_progressive_read_fn().
-    * These functions shouldn't be dependent on global or
-    * static variables if you are decoding several images
-    * simultaneously.  You should store stream specific data
-    * in a separate struct, given as the second parameter,
-    * and retrieve the pointer from inside the callbacks using
-    * the function png_get_progressive_ptr(png_ptr).
-    */
-   png_set_progressive_read_fn( m_psPNGStruct, (void *)this, info_callback, row_callback, end_callback );
+		/* This one's new.  You will need to provide all three
+		 * function callbacks, even if you aren't using them all.
+		 * If you aren't using all functions, you can specify NULL
+		 * parameters.  Even when all three functions are NULL,
+		 * you need to call png_set_progressive_read_fn().
+		 * These functions shouldn't be dependent on global or
+		 * static variables if you are decoding several images
+		 * simultaneously.  You should store stream specific data
+		 * in a separate struct, given as the second parameter,
+		 * and retrieve the pointer from inside the callbacks using
+		 * the function png_get_progressive_ptr(png_ptr).
+		 */
+		png_set_progressive_read_fn( m_psPNGStruct, (void *)this, info_callback, row_callback, end_callback );
+	}
 }
 
 PNGTrans::~PNGTrans()
 {
-    
+	if( m_bWriteMode ) {
+		png_destroy_write_struct( &m_psPNGStruct, (png_infopp)NULL);
+		if( m_pRowBuffer) delete m_pRowBuffer;
+	} else {
+		png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL );
+	}
 }
 
 
@@ -267,21 +318,106 @@ int PNGTrans::EndCallback()
     return 0;
 }
 
+void PNGTrans::WriteCallback( png_bytep pData, int nSize )
+{
+	//dbprintf( "WriteCallback( %p, %d)\n", pData, nSize );
+	AddProcessedData( pData, nSize );
+}
+
+bool PNGTrans::AppendToRowBuffer( void )
+{
+//#define xxx
+#ifdef xxx
+	// Free space in rowbuffer
+	uint32	nSize = m_nRowSize - m_nRowPos;
+	// Adjust to available amount of data (so we don't get a deadlock)
+	nSize = nSize > (uint32)m_cInBuffer.Size() ? m_cInBuffer.Size() : nSize;
+
+	m_cInBuffer.Read( &m_pRowBuffer[ m_nRowPos ], nSize );
+#else
+	uint8	buffer[1024];
+	// Free space in rowbuffer
+	uint32	nSize = m_nRowSize - m_nRowPos;
+	// Adjust to available amount of data (so we don't get a deadlock)
+	nSize = nSize > (uint32)m_cInBuffer.Size() ? m_cInBuffer.Size() : nSize;
+	// Longword alignment, 24 bpp => 32 bpp size conversion
+	nSize = ((int)( nSize / 3 )) * 4;
+	// Adjust to available buffer size
+	nSize = nSize > sizeof( buffer ) ? sizeof(buffer) : nSize;
+
+//	dbprintf("pos: %ld, size: %ld\n", m_nRowPos, m_nRowSize);
+
+
+	// Read and convert RGB32 => RGB24
+//	dbprintf("m_cInBuffer.Read( %p, %ld )...", buffer, nSize);
+	m_cInBuffer.Read( buffer, nSize );
+//	dbprintf("Done!\n");
+	int i, j;
+	for( i = 0, j = 0; i < nSize; i+=4, j+=3 ) {
+		m_pRowBuffer[ m_nRowPos + j     ] = buffer[ i     ];
+		m_pRowBuffer[ m_nRowPos + j + 1 ] = buffer[ i + 1 ];
+		m_pRowBuffer[ m_nRowPos + j + 2 ] = buffer[ i + 2 ];
+	}
+#endif
+	
+	m_nRowPos += nSize;
+	if( m_nRowPos >= m_nRowSize ) {
+		m_nRowPos = 0;
+		return true;
+	} else {
+		return false;
+	}
+}
 
 status_t PNGTrans::AddData( const void* pData, size_t nLen, bool bFinal )
 {
-   if ( setjmp( m_psPNGStruct->jmpbuf ) ) {
-      png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL );
-      return -1;
-   }
-    
-   png_process_data( m_psPNGStruct, m_psPNGInfo, (png_bytep)pData, nLen );
-   return( 0 );
+	if( !m_bWriteMode ) {
+	//	dbprintf("PNG-Read: AddData()\n");
+		if ( setjmp( m_psPNGStruct->jmpbuf ) ) {
+			png_destroy_read_struct( &m_psPNGStruct, &m_psPNGInfo, (png_infopp)NULL );
+			return -1;
+		}
+	    
+		png_process_data( m_psPNGStruct, m_psPNGInfo, (png_bytep)pData, nLen );
+		return 0;
+	} else {
+		m_cInBuffer.Write( pData, nLen );
+	//	dbprintf("PNG-Write: Data added!\n");
+		if( m_eState == STATE_INIT && m_cInBuffer.Size() > (ssize_t)sizeof( BitmapHeader ) ) {
+			m_cInBuffer.Read( &m_sBitmapHeader, sizeof( m_sBitmapHeader ) );
+			m_eState = STATE_FRAMEHDR;
+		}
+
+		if( m_eState == STATE_FRAMEHDR && m_cInBuffer.Size() > (ssize_t)sizeof( BitmapFrameHeader ) ) {
+			m_cInBuffer.Read( &m_sCurrentFrame, sizeof( m_sCurrentFrame ) );
+			// TODO: Verify depth, convert if not RGB32
+			png_set_IHDR( m_psPNGStruct, m_psPNGInfo,
+				m_sCurrentFrame.bf_frame.right - m_sCurrentFrame.bf_frame.left,
+				m_sCurrentFrame.bf_frame.bottom - m_sCurrentFrame.bf_frame.top,
+				8, PNG_COLOR_TYPE_RGB, //_ALPHA,
+				PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE );
+		//	png_set_filler( m_psPNGStruct, 0xff, PNG_FILLER_AFTER);
+			png_set_bgr( m_psPNGStruct );
+			png_write_info( m_psPNGStruct, m_psPNGInfo );
+			AllocRowBuffer( m_sCurrentFrame.bf_frame.right - m_sCurrentFrame.bf_frame.left );
+			m_eState = STATE_READING;
+		}
+		
+		while( m_eState == STATE_READING && m_cInBuffer.Size() ) {
+			if( AppendToRowBuffer() ) {
+				// Overflow = one row is ready
+			//	dbprintf("PNG-Write: Found Row\n");
+				png_write_rows( m_psPNGStruct, &m_pRowBuffer, 1);
+			}
+		}
+
+		if( bFinal ) {
+			png_write_end( m_psPNGStruct, m_psPNGInfo);
+		}
+
+		return 0;
+	}
 }
-
-
-
-
 
 ssize_t PNGTrans::AvailableDataSize()
 {
@@ -290,7 +426,7 @@ ssize_t PNGTrans::AvailableDataSize()
 
 ssize_t PNGTrans::Read( void* pData, size_t nLen )
 {
-    int nCurLen = m_cOutBuffer.Read( pData, nLen );
+    ssize_t nCurLen = m_cOutBuffer.Read( pData, nLen );
     if ( nCurLen == 0 /*&& m_bDone == false*/ ) {
 	return( ERR_SUSPENDED );
     }
@@ -305,7 +441,7 @@ void PNGTrans::Reset()
 {
 }
 
-class PNGTransNode : public TranslatorNode
+class PNGReaderNode : public TranslatorNode
 {
 public:
     virtual int Identify( const std::string& cSrcType, const std::string& cDstType, const void* pData, int nLen ) {
@@ -327,7 +463,24 @@ public:
     }
 };
 
-
+class PNGWriterNode : public TranslatorNode
+{
+public:
+    virtual int Identify( const std::string& cSrcType, const std::string& cDstType, const void* pData, int nLen ) {
+	if ( cSrcType == TRANSLATOR_TYPE_IMAGE && cDstType == "image/png" ) {
+	    return( 0 );
+	}
+	return( TranslatorFactory::ERR_UNKNOWN_FORMAT );
+    }
+    virtual TranslatorInfo GetTranslatorInfo()
+    {
+	static TranslatorInfo sInfo = { TRANSLATOR_TYPE_IMAGE, "image/png", 1.0f };
+	return( sInfo );
+    }
+    virtual Translator*	   CreateTranslator() {
+	return( new PNGTrans( true ) );
+    }
+};
 
 extern "C" {
 #if 0
@@ -336,13 +489,57 @@ extern "C" {
 
 int get_translator_count()
 {
-    return( 1 );
+    return( 2 );
 }
 
 TranslatorNode* get_translator_node( int nIndex )
 {
-    static PNGTransNode sNode;
-    return( &sNode );
+    static PNGWriterNode sWrNode;
+    static PNGReaderNode sRdNode;
+    switch( nIndex ) {
+    	case 1:
+	    return( &sWrNode );
+	default:
+	    return( &sRdNode );
+    }
 }
     
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
