cf20535f7857df59e694af5a971b63d9c2b707cc
vanders <vanders@liqwyd.com>
2002-10-24 11:34:11 +0000

    Version is 0.4.2

--- system/sys/kernel/kernel/version.c
+++ system/sys/kernel/kernel/version.c
@@ -21,7 +21,7 @@
 
 #define MAJOR	0LL
 #define MINOR	4LL
-#define SUB	1LL
+#define SUB	2LL
 
 const int64   g_nKernelVersion	= (MAJOR << 32) | (MINOR << 16) | (SUB);
 const char*   g_pzKernelName	= "kernel.so";
