28af0a1e8e217c94ba21744addc7fb4ac045d799
vanders <vanders@liqwyd.com>
2002-12-30 11:57:30 +0000

    Correctly passes read-only flag to filesystem

--- system/apps/utils/fsutils/mount.c
+++ system/apps/utils/fsutils/mount.c
@@ -7,6 +7,7 @@
 #include <limits.h>
 
 #include <atheos/kernel.h>
+#include <atheos/filesystem.h>
 
 int g_nShowHelp    = 0;
 int g_nShowVersion = 0;
@@ -56,6 +57,7 @@ int main( int argc, char** argv )
     char  zDevPath[PATH_MAX] = "";
     char  zDirPath[PATH_MAX] = "";
     int	nCount;
+	int flags = 0;
   
     while( (c = getopt_long (argc, argv, "hvrwad:t:o:", long_opts, (int *) 0)) != EOF )
     {
@@ -68,6 +70,9 @@ int main( int argc, char** argv )
 		break;
 	    case 'o':
 		pzFsArgs = optarg;
+		break;
+	    case 'r':
+		flags = MNTF_READONLY;
 		break;
 	    case 'v':
 		g_nVerbose = true;
@@ -88,7 +93,7 @@ int main( int argc, char** argv )
 	exit( 0 );
     }
     if ( g_nShowVersion ) {
-	printf( "AtheOS mount V0.1.1 compiled %s\n", __DATE__ );
+	printf( "Syllable mount V0.1.2 compiled %s\n", __DATE__ );
 	exit( 0 );
     }
     nCount = argc - optind;
@@ -110,7 +115,7 @@ int main( int argc, char** argv )
 	printf( "Mount %s %s as %s (fs: %s,fs-args: '%s')\n",
 		(zDevPath[0]) ? zDevPath : "*nodev*", ((g_nReadOnly) ? "ro" : "rw"), zDirPath, pzFsType, (pzFsArgs) ? pzFsArgs : "" );
     }
-    if ( mount( zDevPath, zDirPath, pzFsType, 0, pzFsArgs ) < 0 ) {
+    if ( mount( zDevPath, zDirPath, pzFsType, flags, pzFsArgs ) < 0 ) {
 	fprintf( stderr, "Error: Failed to mount %s: %s\n", zDirPath, strerror( errno ) );
 	return( 1 );
     }
