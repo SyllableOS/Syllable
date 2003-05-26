a359ba8d32d79564c651a37e4c393fb4bd9c8d07
vanders <vanders@liqwyd.com>
2003-05-26 18:53:39 +0000

    Init scripts for bootable floppies and CDs

---
+++ system/build-tools/scripts/sys/basic-init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+boot_mode=$1
+sys_path=$2
+usr_path=$3
+
+sys_path=/boot/atheos
+echo "sys_path:" $sys_path
+
+export PATH=/usr/bin:/boot/atheos/sys/bin/
+export DLL_PATH=@bindir@/lib:./:/boot/atheos/sys/libs:/boot/atheos/sys
+export SYSTEM=AtheOS
+export CC=gcc
+export HOME=/home/root
+export SHELL=bash
+export TEMP=/tmp
+
+aterm &
+
