3b293940ed18921da135b0ebbf9d871be8cf602b
vanders <vanders@liqwyd.com>
2003-08-15 15:53:00 +0000

    Take appserver path from new kernel parameter

--- system/apps/utils/systools/init.c
+++ system/apps/utils/systools/init.c
@@ -1,23 +1,64 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 
-int main()
+int main( int argc, char **argv )
 {
-    symlink( "boot/atheos", "/atheos" );
-    symlink( "atheos/sys", "/system" );
-    symlink( "atheos/sys/bin", "/bin" );
-    symlink( "atheos/usr", "/usr" );
-    symlink( "atheos/etc", "/etc" );
-    symlink( "atheos/var", "/var" );
-    symlink( "atheos/home", "/home" );
-    symlink( "atheos/tmp", "/tmp" );
+	char *pzProgram;
+	char *pzAppserver = "/atheos/sys/appserver", *pzASName;
+	char *pzBootMode = "normal";
+	int c;
 
-    if ( fork() == 0 ) {
-	execl( "/system/appserver", "appserver", NULL );
-	exit(1);
-    }
-    
-    execl( "/system/init.sh", "init.sh", "normal", NULL );
-    printf( "Failed to run init script!\n" );
-    return( 1 );
+	if( (pzProgram = strrchr( *argv, '/' )) != NULL )
+		++pzProgram;
+	else
+		pzProgram = *argv;
+	
+	/* Parse command line */
+	while( (c = getopt( argc, argv, "p:" )) != EOF )
+	{
+		switch( c )
+		{
+			case 'p':
+				pzAppserver = optarg;
+				break;
+				
+			default:
+				break;
+		}
+	}
+	
+	argc -= optind;
+	argv += optind;
+	
+	/* Grab boot mode if specified */
+	if( *argv )
+		pzBootMode = *argv;
+	
+	/* Determine name of appserver executable from path */
+	if( (pzASName = strrchr( pzAppserver, '/' )) != NULL )
+		++pzASName;
+	else
+		pzASName = pzAppserver;
+	
+	symlink( "boot/atheos", "/atheos" );
+	symlink( "atheos/sys", "/system" );
+	symlink( "atheos/sys/bin", "/bin" );
+	symlink( "atheos/usr", "/usr" );
+	symlink( "atheos/etc", "/etc" );
+	symlink( "atheos/var", "/var" );
+	symlink( "atheos/home", "/home" );
+	symlink( "atheos/tmp", "/tmp" );
+
+	if( fork() == 0 ) {
+		execl( pzAppserver, pzASName, NULL );
+		exit( EXIT_FAILURE );
+	}
+	
+	execl( "/system/init.sh", "init.sh", pzBootMode, NULL );
+	printf( "Failed to run init script!\n" );
+	
+	return( EXIT_FAILURE );
 }
