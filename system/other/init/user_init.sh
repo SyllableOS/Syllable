a5353bce02e1ab1fdec6f2395eaf8c647ac3977f
vanders <vanders@liqwyd.com>
2003-09-02 18:37:46 +0000

    Update to latest version

--- system/other/init/user_init.sh
+++ system/other/init/user_init.sh
@@ -7,15 +7,11 @@ dbterm 8 >>/var/log/kernel &
 
 ln -s home/root /root
 
-#Configure the first adaptor (IP address, netmask and gateway)
-#ifconfig eth0 192.168.1.1 255.255.255.0
+# Check for network changes
+netprefs --detect
 
-#Add gateway route(s)
-#route add -i eth0 -g 192.168.1.1 192.168.1.0 255.255.255.0
-
-#Configure the second adaptor
-#ifconfig eth1 192.168.1.2 255.255.255.0
-#route add -i eth1 -g 192.168.1.2 192.168.1.0 255.255.255.0
+# Configure the network
+net_init.sh
 
 # Start various daemons if they are installed
 
