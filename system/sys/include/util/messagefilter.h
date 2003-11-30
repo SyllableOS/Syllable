dbd824970e8890b8b5e74a43b0df524408e956de
digitaliz <digitaliz@users.sourceforge.net>
2003-11-30 17:05:50 +0000

    *** empty log message ***

--- system/sys/include/util/messagefilter.h
+++ system/sys/include/util/messagefilter.h
@@ -1,5 +1,6 @@
-/*  libatheos.so - the highlevel API library for AtheOS
+/*  libsyllable.so - the highlevel API library for Syllable
  *  Copyright (C) 2001 Kurt Skauen
+ *  Copyright (C) 2003 Syllable Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of version 2 of the GNU Library
@@ -32,6 +33,11 @@ class Looper;
 class Message;
 class Handler;
 
+enum MessageFilterResult {
+	MF_DISPATCH_MESSAGE = 0,
+	MF_DISCARD_MESSAGE = 1
+};
+
 /** 
  * \ingroup util
  * \par Description:
@@ -43,32 +49,31 @@ class Handler;
 class MessageFilter
 {
 public:
-    MessageFilter();
-    MessageFilter( uint32 nFilterCode );
+	MessageFilter();
+	MessageFilter( int32 nFilterCode, Handler* pcNewTarget );
     virtual ~MessageFilter();
 
-    virtual bool Filter( Message* pcMessage, Handler** ppcHandler ) = 0;
+    virtual MessageFilterResult Filter( Message* pcMessage, Handler** ppcHandler );
 
-    Handler* GetHandler() const;
-    Looper*  GetLooper() const;
-    bool     FiltersAllCodes() const;
-    uint32   GetFilterCode() const;
-    
+	int32		GetFilterCode() const;
+	void		SetFilterCode( int32 nCode );
+
+	void		SetTarget( Handler* pcTarget );
+	Handler*	GetTarget() const;
+
 private:
-    friend class Handler;
-    friend class Looper;
+	friend class Looper;
+	friend class Handler;
     class Private;
     Private* 				  m;
-    Handler* 				  m_pcHandler;
-    std::list<MessageFilter*>::iterator m_cIterator;
-    uint32   				  m_nFilterCode;
-    bool     				  m_bFiltersAllCodes;
-    bool				  m_bGlobalFilter;
+
+	// Used by Handler & Looper to manage filters:
+	std::list<MessageFilter*>::iterator m_cIterator;	// To find the filter easily in the Handlers list of filters
+	bool			m_bGlobalFilter;	// Added to Looper with AddCommonFilter
+	Handler*		m_pcHandler;		// Handler (or Looper) that the filter is added to
 };
 
 
 } // end of namespace
-
-
 
 #endif // __F_UTIL_MESSAGEFILTER_H__
