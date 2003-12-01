56525199ec0c5400809e514149910a7a940e06f1
digitaliz <digitaliz@users.sourceforge.net>
2003-12-01 20:44:48 +0000

    Changed PRNG to use a Mersenne Twister implementation.

--- system/sys/kernel/kernel/random.c
+++ system/sys/kernel/kernel/random.c
@@ -1,7 +1,9 @@
-
 /*
  *  The Syllable kernel
- *  Copyright (C) 2002 Kristian Van Der Vliet
+ *  Copyright (C) 2003 Syllable Team
+ *  This file uses code that is
+ *  Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura
+ *  (see original copyright notice below)
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of version 2 of the GNU Library
@@ -18,46 +20,130 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*
+	Modifications from the original code (mt19937ar-cok.c) are:
+
+	- genrand_int32() renamed to rand()
+
+	- init_genrand() renamed to seed()
+
+	- default seed is taken from system_time().
+*/
+
 #include <atheos/random.h>
 #include <atheos/types.h>
 #include <atheos/time.h>
 
-/* kseed can be changed at any time by a call to seed() */
-static int64 kseed = 0x12345678;
+/* 
+   A C-program for MT19937, with initialization improved 2002/2/10.
+   Coded by Takuji Nishimura and Makoto Matsumoto.
+   This is a faster version by taking Shawn Cokus's optimization,
+   Matthe Bellew's simplification, Isaku Wada's real version.
 
-/* a is the lower half, b is the upper half */
-static int32 *a = ( int32 * )&kseed;
-static int32 *b = ( int32 * )&kseed + 1;
+   Before using, initialize the state by using init_genrand(seed) 
+   or init_by_array(init_key, key_length).
 
-void seed( int32 seed_val )
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.                          
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     3. The names of its contributors may not be used to endorse or promote 
+        products derived from this software without specific prior written 
+        permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   Any feedback is very welcome.
+   http://www.math.keio.ac.jp/matumoto/emt.html
+   email: matumoto@math.keio.ac.jp
+*/
+
+/* Period parameters */  
+#define N 624
+#define M 397
+#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define UMASK 0x80000000UL /* most significant w-r bits */
+#define LMASK 0x7fffffffUL /* least significant r bits */
+#define MIXBITS(u,v) ( ((u) & UMASK) | ((v) & LMASK) )
+#define TWIST(u,v) ((MIXBITS(u,v) >> 1) ^ ((v)&1UL ? MATRIX_A : 0UL))
+
+static unsigned long state[N]; /* the array for the state vector  */
+static int left = 1;
+static int initf = 0;
+static unsigned long *next;
+
+/* initializes state[N] with a seed */
+void seed(int32 s)
 {
-	kseed ^= seed_val;
-
-	*b ^= *a;
-
-	kseed *= kseed;
-
-	return;
+    int j;
+    state[0]= s & 0xffffffffUL;
+    for (j=1; j<N; j++) {
+        state[j] = (1812433253UL * (state[j-1] ^ (state[j-1] >> 30)) + j); 
+        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+        /* In the previous versions, MSBs of the seed affect   */
+        /* only MSBs of the array state[].                        */
+        /* 2002/01/09 modified by Makoto Matsumoto             */
+        state[j] &= 0xffffffffUL;  /* for >32 bit machines */
+    }
+    left = 1; initf = 1;
 }
 
-uint32 rand( void )
+static void next_state(void)
 {
-	/* a condition where 'a' topmost two bits are set */
-	if ( !( *a & 0xC000000 ) )
-		seed( get_system_time() >> 8 );	/* Reinitialize */
+    unsigned long *p=state;
+    int j;
 
-	/* To avoid zero value after the shift also to give the strengthen no1 */
-	*a ^= *b;
+    /* if init_genrand() has not been called, */
+    /* a default initial seed is used         */
+//    if (initf==0) seed(5489UL);
+	if (initf==0) seed( get_system_time() >> 8 );
 
-	/* Re-confuse ... the strengthen no2 */
-	kseed *= *a;
+    left = N;
+    next = state;
+    
+    for (j=N-M+1; --j; p++) 
+        *p = p[M] ^ TWIST(p[0], p[1]);
 
-	/* Shift ... the strengthen no3 */
-	kseed <<= ( ( *a % 3 ) + 4 );
+    for (j=M; --j; p++) 
+        *p = p[M-N] ^ TWIST(p[0], p[1]);
 
-	/* Avoid zero value */
-	if ( *b == 0 )
-		seed( get_system_time() >> 8 );	/* Reinitialize */
-
-	return ( *b );
+    *p = p[M-N] ^ TWIST(p[0], state[0]);
 }
+
+uint32 rand(void)
+{
+    unsigned long y;
+
+    if (--left == 0) next_state();
+    y = *next++;
+
+    /* Tempering */
+    y ^= (y >> 11);
+    y ^= (y << 7) & 0x9d2c5680UL;
+    y ^= (y << 15) & 0xefc60000UL;
+    y ^= (y >> 18);
+
+    return y;
+}
+
