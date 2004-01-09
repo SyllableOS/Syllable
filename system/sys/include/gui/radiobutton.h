46110a792f25421da55cacf207b8e60ad18350af
digitaliz <digitaliz@users.sourceforge.net>
2004-01-09 20:37:49 +0000

    Added shortcut support. Changed the following classes to use DrawText() and GetTextExtent(). Added/changed focus indicator. Moved private variables to a Private class, or added one for future use. Changed all std::string to os::String, and, where applicable, pzName and pzLabel parameters from char* to String&

--- system/sys/include/gui/radiobutton.h
+++ system/sys/include/gui/radiobutton.h
@@ -1,5 +1,6 @@
-/*  libatheos.so - the highlevel API library for AtheOS
- *  Copyright (C) 1999 - 2001  Kurt Skauen
+/*  libsyllable.so - the highlevel API library for Syllable
+ *  Copyright (C) 1999 - 2001 Kurt Skauen
+ *  Copyright (C) 2003 Syllable Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of version 2 of the GNU Library
@@ -41,26 +42,28 @@ class Bitmap;
 class RadioButton : public Control
 {
 public:
-    RadioButton( Rect cFrame, const char* pzName, const char* pzLabel, Message* pcMessage,
+    RadioButton( Rect cFrame, const String& cName, const String& cLabel, Message* pcMessage,
 		 uint32 nResizeMask = CF_FOLLOW_LEFT | CF_FOLLOW_TOP,
 		 uint32 nFlags  = WID_WILL_DRAW | WID_CLEAR_BACKGROUND );
     ~RadioButton();
     virtual void	AttachedToWindow();
   
-    virtual Point GetPreferredSize( bool bLargest ) const;
-    virtual void	 MouseDown( const Point& cPosition, uint32 nButtons );
-    virtual void	 MouseUp( const Point& cPosition, uint32 nButtons, Message* pcData );
-    virtual void	 Paint( const Rect& cUpdateRect );
-
-//    virtual void	SetValue( int nValue );
+	virtual Point GetPreferredSize( bool bLargest ) const;
+	virtual void	MouseDown( const Point& cPosition, uint32 nButtons );
+	virtual void	MouseUp( const Point& cPosition, uint32 nButtons, Message* pcData );
+	virtual void	KeyDown( const char *pzString, const char *pzRawString, uint32 nQualifiers );
+	virtual void	Paint( const Rect& cUpdateRect );
 
     virtual void PostValueChange( const Variant& cNewValue );
-    virtual void LabelChanged( const std::string& cNewLabel );
+    virtual void LabelChanged( const String& cNewLabel );
     virtual void EnableStatusChanged( bool bIsEnabled );
     virtual bool Invoked( Message* pcMessage );
     
 private:
     static Bitmap* s_pcBitmap;
+    
+    class Private;
+    Private *m;
 };
 
 }
