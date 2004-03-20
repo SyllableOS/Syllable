e661a88845a9da72ee9c845c69d1caad8f8be367
digitaliz <digitaliz@users.sourceforge.net>
2004-03-20 13:48:29 +0000

    Moved translators to /system/drivers/translators

--- system/sys/appserver/libsyllable/translation/translator.cpp
+++ system/sys/appserver/libsyllable/translation/translator.cpp
@@ -1,3 +1,4 @@
+
 /*  libatheos.so - the highlevel API library for AtheOS
  *  Copyright (C) 2001  Kurt Skauen
  *
@@ -33,157 +34,184 @@
 using namespace os;
 
 
-static TranslatorFactory* g_pcDefaultFactory = NULL;
+static TranslatorFactory *g_pcDefaultFactory = NULL;
 
 
 struct Plugin
 {
-    int	m_nLibrary;
-    std::vector<TranslatorNode*> m_cNodes;
+	int m_nLibrary;
+	  std::vector < TranslatorNode * >m_cNodes;
 };
 
 struct TranslatorFactory::Internal
 {
-    std::vector<Plugin*>	 m_cPlugins;
-    std::vector<TranslatorNode*> m_cNodes;
+	std::vector < Plugin * >m_cPlugins;
+	std::vector < TranslatorNode * >m_cNodes;
 };
 
 TranslatorFactory::TranslatorFactory()
 {
-    m = new Internal;
+	m = new Internal;
 }
 
 TranslatorFactory::~TranslatorFactory()
 {
-    delete m;
+	delete m;
 }
 
 typedef int get_translator_count();
-typedef TranslatorNode* get_translator_node( int nIndex );
+typedef TranslatorNode *get_translator_node( int nIndex );
 
 void TranslatorFactory::LoadAll()
 {
-    try {
-	Directory cDir( "/system/translators" );
+	try
+	{
+		Directory cDir( "/system/drivers/translators" );
 
-	String cName;
-	while( cDir.GetNextEntry( &cName ) == 1 ) {
-	    if ( cName == "." || cName == ".." ) {
-		continue;
-	    }
-	    String cPath( "/system/translators/" );
-	    cPath += cName;
+		String cName;
 
-	    int nLib = load_library( cPath.c_str(), 0 );
-	    if ( nLib < 0 ) {
-		dbprintf( "failed to load translator %s\n", cPath.c_str() );
-		continue;
-	    }
-	    get_translator_count* pGetTransCount;
-	    get_translator_node* pGetTrans;
+		while( cDir.GetNextEntry( &cName ) == 1 )
+		{
+			if( cName == "." || cName == ".." )
+			{
+				continue;
+			}
+			String cPath( "/system/drivers/translators/" );
 
-	    int nError;
-	    
-	    nError = get_symbol_address( nLib, "get_translator_count", -1, (void**) &pGetTransCount );
-	    if ( nError < 0 ) {
-		dbprintf( "Translator %s does not export entry point get_translator_count()\n", cName.c_str() );
-		unload_library( nLib );
-		continue;
-	    }
-	    nError = get_symbol_address( nLib, "get_translator_node", -1, (void**) &pGetTrans );
-	    if ( nError < 0 ) {
-		dbprintf( "Translator %s does not export entry point get_translator_node()\n", cName.c_str() );
-		unload_library( nLib );
-		continue;
-	    }
-	    int nTransCount = pGetTransCount();
-	    if ( nTransCount == 0 ) {
-		unload_library( nLib );
-		continue;
-	    }
-	    Plugin* pcPlugin = new Plugin;
-	    pcPlugin->m_nLibrary = nLib;
-	    m->m_cPlugins.push_back( pcPlugin );
-	    
-	    for ( int i = 0 ; i < nTransCount ; ++i ) {
-		TranslatorNode* pcNode = pGetTrans( i );
-		if ( pcNode != NULL ) {
-		    pcPlugin->m_cNodes.push_back( pcNode );
-		    m->m_cNodes.push_back( pcNode );
+			cPath += cName;
+
+			int nLib = load_library( cPath.c_str(), 0 );
+
+			if( nLib < 0 )
+			{
+				dbprintf( "failed to load translator %s\n", cPath.c_str() );
+				continue;
+			}
+			get_translator_count *pGetTransCount;
+			get_translator_node *pGetTrans;
+
+			int nError;
+
+			nError = get_symbol_address( nLib, "get_translator_count", -1, ( void ** )&pGetTransCount );
+			if( nError < 0 )
+			{
+				dbprintf( "Translator %s does not export entry point get_translator_count()\n", cName.c_str() );
+				unload_library( nLib );
+				continue;
+			}
+			nError = get_symbol_address( nLib, "get_translator_node", -1, ( void ** )&pGetTrans );
+			if( nError < 0 )
+			{
+				dbprintf( "Translator %s does not export entry point get_translator_node()\n", cName.c_str() );
+				unload_library( nLib );
+				continue;
+			}
+			int nTransCount = pGetTransCount();
+
+			if( nTransCount == 0 )
+			{
+				unload_library( nLib );
+				continue;
+			}
+			Plugin *pcPlugin = new Plugin;
+
+			pcPlugin->m_nLibrary = nLib;
+			m->m_cPlugins.push_back( pcPlugin );
+
+			for( int i = 0; i < nTransCount; ++i )
+			{
+				TranslatorNode *pcNode = pGetTrans( i );
+
+				if( pcNode != NULL )
+				{
+					pcPlugin->m_cNodes.push_back( pcNode );
+					m->m_cNodes.push_back( pcNode );
+				}
+			}
 		}
-	    }
 	}
-    } catch(...) {
-    }
+	catch( ... )
+	{
+	}
 }
 
-status_t TranslatorFactory::FindTranslator( const String& cSrcType, const String& cDstType,
-					    const void* pData, size_t nLen, Translator** ppcTrans )
+status_t TranslatorFactory::FindTranslator( const String &cSrcType, const String &cDstType, const void *pData, size_t nLen, Translator ** ppcTrans )
 {
-    float vBestQuality = -1000.0f;
-    TranslatorNode* pcBestTrans = NULL;
-    int nError = ERR_UNKNOWN_FORMAT;
-    
-    for ( uint i = 0 ; i < m->m_cNodes.size() ; ++i ) {
-	TranslatorNode* pcNode = m->m_cNodes[i];
-	TranslatorInfo sInfo   = pcNode->GetTranslatorInfo();
+	float vBestQuality = -1000.0f;
+	TranslatorNode *pcBestTrans = NULL;
+	int nError = ERR_UNKNOWN_FORMAT;
 
-	if ( !( sInfo.dest_type == cDstType ) ) {
-	    continue;
-	}
+	for( uint i = 0; i < m->m_cNodes.size(); ++i )
+	{
+		TranslatorNode *pcNode = m->m_cNodes[i];
+		TranslatorInfo sInfo = pcNode->GetTranslatorInfo();
 
-	int nResult = pcNode->Identify( cSrcType, cDstType, pData, nLen );
+		if( !( sInfo.dest_type == cDstType ) )
+		{
+			continue;
+		}
 
-	if ( nResult == 0 ) {
-	    if ( sInfo.quality > vBestQuality ) {
-		vBestQuality = sInfo.quality;
-		pcBestTrans = pcNode;
-	    }
-	} else if ( nResult == ERR_NOT_ENOUGH_DATA ) {
-	    nError = ERR_NOT_ENOUGH_DATA;
+		int nResult = pcNode->Identify( cSrcType, cDstType, pData, nLen );
+
+		if( nResult == 0 )
+		{
+			if( sInfo.quality > vBestQuality )
+			{
+				vBestQuality = sInfo.quality;
+				pcBestTrans = pcNode;
+			}
+		}
+		else if( nResult == ERR_NOT_ENOUGH_DATA )
+		{
+			nError = ERR_NOT_ENOUGH_DATA;
+		}
 	}
-    }
-    if ( pcBestTrans != NULL ) {
-	*ppcTrans = pcBestTrans->CreateTranslator();
-	if ( *ppcTrans != NULL ) {
-	    nError = 0;
-	} else {
-	    nError = ERR_NO_MEM;
+	if( pcBestTrans != NULL )
+	{
+		*ppcTrans = pcBestTrans->CreateTranslator();
+		if( *ppcTrans != NULL )
+		{
+			nError = 0;
+		}
+		else
+		{
+			nError = ERR_NO_MEM;
+		}
 	}
-    }
-    return( nError );
+	return ( nError );
 }
 
 int TranslatorFactory::GetTranslatorCount()
 {
-    return( m->m_cNodes.size() );
+	return ( m->m_cNodes.size() );
 }
 
-TranslatorNode* TranslatorFactory::GetTranslatorNode( int nIndex )
+TranslatorNode *TranslatorFactory::GetTranslatorNode( int nIndex )
 {
-    return( m->m_cNodes[nIndex] );
+	return ( m->m_cNodes[nIndex] );
 }
 
 TranslatorInfo TranslatorFactory::GetTranslatorInfo( int nIndex )
 {
-    return( m->m_cNodes[nIndex]->GetTranslatorInfo() );
+	return ( m->m_cNodes[nIndex]->GetTranslatorInfo() );
 }
 
-Translator* TranslatorFactory::CreateTranslator( int nIndex )
+Translator *TranslatorFactory::CreateTranslator( int nIndex )
 {
-    return( m->m_cNodes[nIndex]->CreateTranslator() );
+	return ( m->m_cNodes[nIndex]->CreateTranslator() );
 }
 
 
 
 
-TranslatorFactory* TranslatorFactory::GetDefaultFactory()
+TranslatorFactory *TranslatorFactory::GetDefaultFactory()
 {
-    if ( g_pcDefaultFactory == NULL ) {
-	g_pcDefaultFactory = new TranslatorFactory();
-	g_pcDefaultFactory->LoadAll();
-    }
-    return( g_pcDefaultFactory );
+	if( g_pcDefaultFactory == NULL )
+	{
+		g_pcDefaultFactory = new TranslatorFactory();
+		g_pcDefaultFactory->LoadAll();
+	}
+	return ( g_pcDefaultFactory );
 }
 
 
@@ -202,29 +230,34 @@ TranslatorFactory* TranslatorFactory::GetDefaultFactory()
 
 struct Translator::Internal
 {
-	Internal() {
+	Internal()
+	{
 		m_pcDataTarget = NULL;
 		m_nBufSize = 0;
 		m_pnBuffer = NULL;
 	}
-	~Internal() {
+	~Internal()
+	{
 		if( m_pnBuffer )
-			delete []m_pnBuffer;
+			delete[]m_pnBuffer;
 	}
-	void ResizeBuffer( uint32 nNewSize ) {
-		uint8* tmp = new uint8[ nNewSize ];
+	void ResizeBuffer( uint32 nNewSize )
+	{
+		uint8 *tmp = new uint8[nNewSize];
+
 		memcpy( tmp, m_pnBuffer, m_nBufSize );
 		m_nBufSize = nNewSize;
-		delete []tmp;
+		delete[]tmp;
 		m_pnBuffer = tmp;
 	}
-    Messenger	       m_cMsgTarget;
-    Message	       m_cMessage;
-    DataReceiver*      m_pcDataTarget;
-    bool	       m_bSendData;
+	Messenger m_cMsgTarget;
+	Message m_cMessage;
+	DataReceiver *m_pcDataTarget;
+	bool m_bSendData;
+
 //    std::vector<uint8> m_cBuffer;
-	uint8*		m_pnBuffer;
-	uint32		m_nBufSize;
+	uint8 *m_pnBuffer;
+	uint32 m_nBufSize;
 };
 
 
@@ -232,70 +265,74 @@ struct Translator::Internal
 
 Translator::Translator()
 {
-    m = new Internal;
+	m = new Internal;
 }
 
 Translator::~Translator()
 {
-    delete m;
+	delete m;
 }
 
-void Translator::SetMessage( const Message& cMsg )
+void Translator::SetMessage( const Message & cMsg )
 {
-    m->m_cMessage = cMsg;
+	m->m_cMessage = cMsg;
 }
 
-void Translator::SetTarget( const Messenger& cTarget, bool bSendData )
+void Translator::SetTarget( const Messenger & cTarget, bool bSendData )
 {
-    m->m_cMsgTarget = cTarget;
-    m->m_bSendData = bSendData;
+	m->m_cMsgTarget = cTarget;
+	m->m_bSendData = bSendData;
 }
 
-void Translator::SetTarget( DataReceiver* pcTarget )
+void Translator::SetTarget( DataReceiver * pcTarget )
 {
-    m->m_pcDataTarget = pcTarget;
+	m->m_pcDataTarget = pcTarget;
 }
 
-status_t Translator::AddData( const void* pData, size_t nLen, bool bFinal )
+status_t Translator::AddData( const void *pData, size_t nLen, bool bFinal )
 {
 //    size_t nOldSize = m->m_cBuffer.size();
 	size_t nOldSize = m->m_nBufSize;
+
 	m->ResizeBuffer( nOldSize + nLen );
 //    m->m_cBuffer.resize( nOldSize + nLen );
 //    memcpy( (void*)(m->m_cBuffer.begin() + nOldSize), pData, nLen );
 	memcpy( m->m_pnBuffer + nOldSize, pData, nLen );
 
-	return( DataAdded( m->m_pnBuffer, m->m_nBufSize, bFinal ) );
+	return ( DataAdded( m->m_pnBuffer, m->m_nBufSize, bFinal ) );
 //    return( DataAdded( m->m_cBuffer.c_str(), m->m_cBuffer.size(), bFinal ) );
 }
 
-status_t Translator::DataAdded( void* pData, size_t nLen, bool bFinal )
+status_t Translator::DataAdded( void *pData, size_t nLen, bool bFinal )
 {
-    return( 0 );
+	return ( 0 );
 }
 
-void Translator::Invoke( void* pData, size_t nSize, bool bFinal )
+void Translator::Invoke( void *pData, size_t nSize, bool bFinal )
 {
-    if ( m->m_cMsgTarget.IsValid() ) {
-	Message cMsg( m->m_cMessage );
+	if( m->m_cMsgTarget.IsValid() )
+	{
+		Message cMsg( m->m_cMessage );
 
-	cMsg.AddInt32( "size", nSize );
-	cMsg.AddBool( "final", bFinal );
-	
-	if ( m->m_bSendData ) {
-	    cMsg.AddData( "data", T_ANY_TYPE, pData, nSize );
+		cMsg.AddInt32( "size", nSize );
+		cMsg.AddBool( "final", bFinal );
+
+		if( m->m_bSendData )
+		{
+			cMsg.AddData( "data", T_ANY_TYPE, pData, nSize );
+		}
+		m->m_cMsgTarget.SendMessage( &cMsg );
 	}
-	m->m_cMsgTarget.SendMessage( &cMsg );
-    }
-    if ( m->m_pcDataTarget != NULL ) {
-	m->m_pcDataTarget->DataReady( pData, nSize, bFinal );
-    }
+	if( m->m_pcDataTarget != NULL )
+	{
+		m->m_pcDataTarget->DataReady( pData, nSize, bFinal );
+	}
 }
 
 
 TranslatorNode::TranslatorNode()
 {
-    m = NULL;
+	m = NULL;
 }
 
 TranslatorNode::~TranslatorNode()
