f1fe693334e1ebfd05cbbfbc496efc76f9cce628
vanders <vanders@liqwyd.com>
2004-04-29 20:01:36 +0000

    VMWare video driver

---
+++ system/sys/kernel/drivers/graphics/vmware/vmware_pci_ids.h
@@ -0,0 +1,16 @@
+#ifndef __VMWARE_PCI_IDS_H__
+#define __VMWARE_PCI_IDS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PCI_VENDOR_ID_VMWARE 			0x15AD
+#define PCI_DEVICE_ID_VMWARE_SVGA2		0x0405
+#define PCI_DEVICE_ID_VMWARE_SVGA		0x0710
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NV_PCI_IDS_H__ */
