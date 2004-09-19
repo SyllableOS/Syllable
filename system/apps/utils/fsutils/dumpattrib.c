6b66f65bdeb9da704b70f80c313654f1d64852cf
vanders <vanders@liqwyd.com>
2004-09-19 16:37:57 +0000

    New dumpattrib utility, lsattribs bug fixes

---
+++ system/apps/utils/fsutils/dumpattrib.c
@@ -0,0 +1,65 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <atheos/fs_attribs.h>
+
+int main(int argc, char *argv[])
+{
+	int nRetVal = 1;
+
+	if (argc < 4)
+		printf("usage: %s path attr_name dump_file\n", argv[0]);
+
+	else
+	{
+		register char *pBuffer;
+		register int hFd, hFdCopy;
+		struct attr_info sAttrInfo;
+
+		if ((hFd = open(argv[1], O_RDWR | O_NOTRAVERSE)) >= 0)
+		{
+			if ((hFdCopy = open(argv[3], O_RDWR | O_CREAT | O_TRUNC | O_NOTRAVERSE)) >= 0)
+			{
+				if (stat_attr( hFd, argv[2], &sAttrInfo) == 0)
+				{
+					if ((pBuffer = (char *)malloc(sAttrInfo.ai_size)) != NULL)
+					{
+						if (read_attr(hFd, argv[2], sAttrInfo.ai_type, pBuffer, 0, sAttrInfo.ai_size) == sAttrInfo.ai_size)
+						{
+							if (write(hFdCopy, pBuffer, sAttrInfo.ai_size) == sAttrInfo.ai_size)
+								nRetVal = 0;
+							else
+								fprintf(stderr, "Failed to dump attrib %s\n", argv[2]);
+						}
+
+						else
+							fprintf(stderr, "Failed to read attrib %s\n", argv[2]);
+
+						free(pBuffer);
+					}
+
+					else
+						fprintf(stderr, "Out of memory!\n");
+				}
+
+				else
+					fprintf(stderr, "Failed to stat attrib %s\n", argv[2]);
+
+				close(hFdCopy);
+			}
+
+			else
+				fprintf(stderr, "Failed to open destination file %s: %s\n", argv[3], strerror(errno));
+
+			close(hFd);
+		}
+
+		else
+			fprintf(stderr, "Failed to open %s: %s\n", argv[1], strerror(errno));
+	}
+
+	return nRetVal;
+}
--- system/apps/utils/fsutils/lsattribs.c
+++ system/apps/utils/fsutils/lsattribs.c
@@ -1,100 +1,123 @@
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <assert.h>
 #include <errno.h>
-
 #include <atheos/fs_attribs.h>
-
-void print_usage( const char* pzName )
-{
-    printf( "usage: %s path\n", pzName );
-}
-
-void read_string_attr( int nFile, const char *pzName, char *pzBuffer, int nSize )
-{
-    int nLen;
-
-    if ( ( nLen = read_attr( nFile, pzName, ATTR_TYPE_STRING, pzBuffer, 0, nSize - 1 ) ) > 0 )
-    {
-	pzBuffer[ nLen + 1 ] = '\0';
-    } else {
-	strcpy( pzBuffer, "**error**" );
-    }
-}
 
 int main( int argc, char** argv )
 {
-    DIR* pDir;
-    struct dirent* psEntry;
-    int    nFile;
+	DIR* pDir;
+	struct dirent* psEntry;
+	int nFile;
   
-    if ( argc < 2 ) {
-	print_usage( argv[0] );
-	return( 1 );
-    }
-
-    nFile = open( argv[1], O_RDWR | O_NOTRAVERSE );
-
-    if ( nFile < 0 ) {
-	printf( "Failed to open %s: %s\n", argv[1], strerror( errno ) );
-	return( 1 );
-    }
-  
-    pDir = open_attrdir( nFile );
-
-    if ( NULL == pDir ) {
-	printf( "Failed to open attrib dir\n" );
-	return( 1 );
-    }
-  
-    while( (psEntry = read_attrdir( pDir )) )
-    {
-	char zBuffer[1024];
-    
-	attr_info_s sInfo;
-    
-	if ( stat_attr( nFile, psEntry->d_name, &sInfo ) == 0 )
-	{
-	    printf( "%s - size = %d, type = %d ", psEntry->d_name, (int)sInfo.ai_size, sInfo.ai_type );
-
-	    switch( sInfo.ai_type )
-	    {
-	    	case ATTR_TYPE_STRING:
-	    		read_string_attr( nFile, psEntry->d_name, zBuffer, sizeof( zBuffer ) );
-	    		printf( "[ATTR_TYPE_STRING] - \"%s\"", zBuffer );
-	    		break;
-	    	case ATTR_TYPE_NONE:
-	    		printf( "[ATTR_TYPE_NONE (Raw binary data)]" );
-	    		break;
-	    	case ATTR_TYPE_INT32:
-	    		printf( "[ATTR_TYPE_INT32]" );
-	    		break;
-	    	case ATTR_TYPE_INT64:
-	    		printf( "[ATTR_TYPE_INT64]" );
-	    		break;
-	    	case ATTR_TYPE_FLOAT:
-	    		printf( "[ATTR_TYPE_FLOAT]" );
-	    		break;
-	    	case ATTR_TYPE_DOUBLE:
-	    		printf( "[ATTR_TYPE_DOUBLE]" );
-	    		break;
-	    	default:
-	    		printf( "[Unknown]" );
-	    		break;
-	    }
-	    printf( "\n" );
-	} else {
-	    printf( "Failed to stat attrib %s::%s\n", argv[1], psEntry->d_name );
+	if ( argc < 2 ) {
+		printf( "usage: %s path\n", argv[0] );
+		return( 1 );
 	}
-    }
 
-    close_attrdir( pDir );
-    return( 0 );
+	nFile = open( argv[1], O_RDWR | O_NOTRAVERSE );
+
+	if ( nFile < 0 ) {
+		printf( "Failed to open %s: %s\n", argv[1], strerror( errno ) );
+		return( 1 );
+	}
+
+	pDir = open_attrdir( nFile );
+
+	if ( NULL == pDir ) {
+		printf( "Failed to open attrib dir\n" );
+		close ( nFile );
+		return( 1 );
+	}
+  
+	while( ( psEntry = read_attrdir( pDir ) ) )
+	{
+		char zBuffer[1024];
+		attr_info_s sInfo;
+
+		union {
+			int32 m_nInt32;
+			int64 m_nInt64;
+			float m_vFloat;
+			double m_vDouble;
+		} sTypes;
+
+		register ssize_t nLen;
+
+		if ( stat_attr( nFile, psEntry->d_name, &sInfo ) == 0 )
+		{
+			printf( "%s - size = %d, type = %d ", psEntry->d_name, (int)sInfo.ai_size, sInfo.ai_type );
+
+			switch( sInfo.ai_type )
+			{
+				case ATTR_TYPE_NONE:
+	    			printf( "[ATTR_TYPE_NONE (Raw binary data)]" );
+	    			break;
+
+	    		case ATTR_TYPE_INT32:
+				{
+					if ( ( sInfo.ai_size == sizeof( int32 ) ) && ( ( nLen = read_attr( nFile, psEntry->d_name, ATTR_TYPE_INT32, &(sTypes.m_nInt32), 0, sInfo.ai_size ) ) == sInfo.ai_size ) )
+						sprintf( zBuffer, "%li", sTypes.m_nInt32 );
+					else
+						strcpy(zBuffer, "**error**");
+	    			printf( "[ATTR_TYPE_INT32] - \"%s\"", zBuffer );
+	    			break;
+				}
+
+	    		case ATTR_TYPE_INT64:
+				{
+					if ( ( sInfo.ai_size == sizeof( int64 ) ) && ( ( nLen = read_attr( nFile, psEntry->d_name, ATTR_TYPE_INT64, &(sTypes.m_nInt64), 0, sInfo.ai_size ) ) == sInfo.ai_size ) )
+						sprintf( zBuffer, "%lli", sTypes.m_nInt64 );
+					else
+						strcpy(zBuffer, "**error**");
+	    			printf( "[ATTR_TYPE_INT64] - \"%s\"", zBuffer );
+	    			break;
+				}
+
+	    		case ATTR_TYPE_FLOAT:
+				{
+					if ( ( sInfo.ai_size == sizeof( float ) ) && ( ( nLen = read_attr( nFile, psEntry->d_name, ATTR_TYPE_FLOAT, &(sTypes.m_vFloat), 0, sInfo.ai_size ) ) == sInfo.ai_size ) )
+						sprintf( zBuffer, "%f", sTypes.m_vFloat );
+					else
+						strcpy(zBuffer, "**error**");
+	    			printf( "[ATTR_TYPE_FLOAT] - \"%s\"", zBuffer );
+	    			break;
+	    		}
+
+	    		case ATTR_TYPE_DOUBLE:
+				{
+					if ( ( sInfo.ai_size == sizeof( double ) ) && ( ( nLen = read_attr( nFile, psEntry->d_name, ATTR_TYPE_DOUBLE, &(sTypes.m_vDouble), 0, sInfo.ai_size ) ) == sInfo.ai_size ) )
+						sprintf( zBuffer, "%lf", sTypes.m_vDouble );
+					else
+						strcpy(zBuffer, "**error**");
+	    			printf( "[ATTR_TYPE_DOUBLE] - \"%s\"", zBuffer );
+	    			break;
+				}
+
+				case ATTR_TYPE_STRING:
+				{
+					if ( ( nLen = read_attr( nFile, psEntry->d_name, ATTR_TYPE_STRING, zBuffer, 0, sizeof( zBuffer ) - 1 ) ) == sInfo.ai_size )
+						zBuffer[nLen] = '\0';
+					else
+						strcpy(zBuffer, "**error**");
+					printf( "[ATTR_TYPE_STRING] - \"%s\"", zBuffer );
+					break;
+				}
+
+	    		default:
+	    			printf( "[UNKNOWN]" );
+	    			break;
+			}
+
+			printf( "\n" );
+
+		} else
+			printf( "Failed to stat attrib %s::%s\n", argv[1], psEntry->d_name );
+	}
+
+	close_attrdir( pDir );
+	close( nFile );
+	return( 0 );  
 }
-
-
-
-
-
-
-
