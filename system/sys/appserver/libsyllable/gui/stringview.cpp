1027a9003abc1de85b418f162846921e272dc61d
digitaliz <digitaliz@users.sourceforge.net>
2005-03-06 08:01:32 +0000

    Added option to draw a border. Uses BgColor of parent Size calculation improved

--- system/sys/appserver/libsyllable/gui/stringview.cpp
+++ system/sys/appserver/libsyllable/gui/stringview.cpp
@@ -33,12 +33,14 @@ class StringView::Private
 	Private() {
 		m_cMinSize = IPoint( 0, 0 );
 		m_cMaxSize = IPoint( 0, 0 );
+		m_bHasBorder = false;
 	}
 
 	String		m_cString;
 	IPoint		m_cMinSize;
 	IPoint		m_cMaxSize;
     alignment	m_eAlign;
+    bool		m_bHasBorder;
 };
 
 //----------------------------------------------------------------------------
@@ -52,8 +54,8 @@ StringView::StringView( Rect cFrame, const String& cName, const String& cString,
 {
 	m = new Private;
 	m->m_eAlign = eAlign;
-	SetFgColor( 0, 0, 0 );
 	SetString( cString );
+	SetFgColor( 0, 0, 0 );
 }
 
 //----------------------------------------------------------------------------
@@ -94,6 +96,16 @@ const String& StringView::GetString( void ) const
 	return ( m->m_cString );
 }
 
+bool StringView::HasBorder() const
+{
+	return m->m_bHasBorder;
+}
+
+void StringView::SetRenderBorder( bool bRender )
+{
+	m->m_bHasBorder = bRender;
+}
+
 void StringView::SetMinPreferredSize( int nWidthChars, int nHeightChars )
 {
 	m->m_cMinSize.x = nWidthChars;
@@ -116,19 +128,28 @@ void StringView::SetMaxPreferredSize( int nWidthChars, int nHeightChars )
 Point StringView::GetPreferredSize( bool bLargest ) const
 {
 	Point cExt = GetTextExtent( m->m_cString );
+	Point cBorder( 0, 0 );
+
+	font_height	sHeight;
+	GetFontHeight( &sHeight );
+
+	if( m->m_bHasBorder ) {
+		cBorder.x += 4;
+		cBorder.y += 4;
+	}
 
 	if( bLargest )
 	{
 		return ( Point(
-			( ( m->m_cMaxSize.x > 0 ) ? float ( m->m_cMaxSize.x ) * GetStringWidth( "M" ) : COORD_MAX ),
-			( ( m->m_cMaxSize.y > 0 ) ? float ( m->m_cMaxSize.y ) * GetStringWidth( "M" ) : COORD_MAX ) ) );
+			( ( m->m_cMaxSize.x > 0 ) ? float ( m->m_cMaxSize.x ) * GetStringWidth( "M" ) +  + cBorder.x : COORD_MAX ),
+			( ( m->m_cMaxSize.y > 0 ) ? float ( m->m_cMaxSize.y ) * ( sHeight.ascender + sHeight.descender )  + cBorder.y : COORD_MAX ) ) );
 		/* FIXME: This code assumes that M is the widest character. */
 	}
 	else
 	{
 		return ( Point(
-			( ( m->m_cMinSize.x > 0 ) ? float ( m->m_cMinSize.x ) * GetStringWidth( "M" ) : cExt.x ),
-			( ( m->m_cMinSize.y > 0 ) ? float ( m->m_cMinSize.y ) * GetStringWidth( "M" ) : cExt.y ) ) );
+			( ( m->m_cMinSize.x > 0 ) ? float ( m->m_cMinSize.x ) * GetStringWidth( "M" )  + cBorder.x : cExt.x + cBorder.x ),
+			( ( m->m_cMinSize.y > 0 ) ? float ( m->m_cMinSize.y ) * ( sHeight.ascender + sHeight.descender ) + cBorder.y : cExt.y + cBorder.y ) ) );
 		/* FIXME: This code assumes that M is the widest character. */
 	}
 }
@@ -154,7 +175,13 @@ void StringView::Paint( const Rect & cUpdateRect )
 {
 	Rect cBounds = GetBounds();
 
-	FillRect( cBounds, get_default_color( COL_NORMAL ) );
+	if( m->m_bHasBorder ) {
+		SetEraseColor( GetBgColor() );
+		DrawFrame( cBounds, FRAME_RECESSED | FRAME_THIN );
+		cBounds.Resize( 2, 2, -2, -2 );
+	} else {
+		FillRect( cBounds, GetBgColor() );
+	}
 
 	if( m->m_eAlign == ALIGN_LEFT )
 	{
