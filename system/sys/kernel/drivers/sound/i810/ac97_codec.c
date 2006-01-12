c21c2eba2bb7de9576ac268cbfb63408ab8bbf80
vanders <vanders@liqwyd.com>
2006-01-12 22:45:53 +0000

    Missed this bitops.h change in the previous update.

--- system/sys/kernel/drivers/sound/i810/ac97_codec.c
+++ system/sys/kernel/drivers/sound/i810/ac97_codec.c
@@ -67,9 +67,9 @@
 #include <atheos/soundcard.h>
 #include <atheos/spinlock.h>
 #include <atheos/list.h>
+#include <atheos/bitops.h>
 #include <macros.h>
 
-#include "bitops.h"
 #include "linuxcomp.h"
 #include "ac97_codec.h"
 
