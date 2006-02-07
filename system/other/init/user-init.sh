7bca886a43c19b34f46d9568b0523588e6b41e24
kajdevos <kajdevos@users.sourceforge.net>
2006-02-07 00:14:26 +0000

    Added mounting example

--- system/other/init/user-init.sh
+++ system/other/init/user-init.sh
@@ -2,3 +2,10 @@
 # Perform system specific initialisation.
 # Please add any additional configuration here.
 
+# This is an example for mounting an extra volume, assuming that
+# your first partition is a Windows FAT partition. Uncomment the
+# following lines and adjust the partition number to your system.
+#
+#mkdir /windows
+#mount /dev/disk/ata/hda/0 /windows
+
