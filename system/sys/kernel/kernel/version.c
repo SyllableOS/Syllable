0e5143da5c10a045ec4e0d8dc856e41870a5e6cb
vanders <vanders@liqwyd.com>
2006-05-14 10:22:27 +0000

    Update to 0.6.1 Don't inherit CFLAGS; the kernel must be built with very specific flags for it to work

--- system/sys/kernel/kernel/version.c
+++ system/sys/kernel/kernel/version.c
@@ -22,7 +22,7 @@
 
 #define MAJOR	0LL
 #define MINOR	6LL
-#define RELEASE	0LL
+#define RELEASE	1LL
 
 const int64 g_nKernelVersion = ( MAJOR << 32 ) | ( MINOR << 16 ) | ( RELEASE );
 const char *g_pzKernelName = "kernel.so";
