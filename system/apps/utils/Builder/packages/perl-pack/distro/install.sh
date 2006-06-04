1fefe24a1905ad879b24593a01ef9c407c649bf2
kajdevos <kajdevos@users.sourceforge.net>
2006-06-04 23:15:59 +0000

    Added PERL Pit meta-package

---
+++ system/apps/utils/Builder/packages/perl-pack/distro/install.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+packages='perl autoconf automake'
+
+if [ "$USER" != "root" ]
+then
+	echo "The installation is not being run by the administrative user."
+	echo "Installation aborted."
+	echo "You can log in as the \"root\" user and try again."
+	exit 1
+fi
+
+echo "This will install the packages contained in The PERL Pit."
+echo "Previously installed packages of the same name will be removed first."
+echo ""
+read -p "Do you want to continue (y/N)? " -e answer
+
+if [ "$answer" != "y" ]
+then
+	echo "Installation aborted."
+	exit 1
+fi
+
+echo ""
+
+for package in $packages
+do
+	if [ -e "/usr/$package" ]
+	then
+		echo "Uninstalling existing /usr/$package"
+		pkgmanager -r /usr/$package
+		rm -r /usr/$package
+		sync
+	fi
+done
+
+echo ""
+
+for package in `ls *.zip`
+do
+	echo "Installing $package"
+	unzip $package -d /usr
+	sync
+done
+
+for package in $packages
+do
+	echo ""
+	echo "Registering $package"
+	pkgmanager -a /usr/$package
+	sync
+done
+
+echo ""
+echo "Done"
+
