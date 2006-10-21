cb3664c0e3c76922a8ae6c2ca47791688ddcd1ff
kajdevos <kajdevos@users.sourceforge.net>
2006-10-21 19:40:29 +0000

    Added language setting example

--- system/other/init/user-early-init.sh
+++ system/other/init/user-early-init.sh
@@ -3,3 +3,6 @@
 # Please add any additional configuration here.
 # This will run before user login and may include environment variables.
 
+# Set your language here if you want the command-line environment to be translated:
+#export LANG=nl_NL.UTF-8
+#export LANGUAGE=nl_NL:nl
