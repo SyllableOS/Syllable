250e47949f09dfa50636f83d1b91e9b17a3b84a3
kajdevos <kajdevos@users.sourceforge.net>
2006-10-21 06:18:07 +0000

    Rationalized paths further

--- system/other/init/init.sh
+++ system/other/init/init.sh
@@ -17,7 +17,6 @@ dbterm 8 >>/var/log/kernel &
 
 # Add a few additional symlinks which simplify navigation around the
 # filesystem
-ln -s /usr/glibc/include /include
 ln -s /atheos/Applications /Applications
 ln -s /home/root /root
 
