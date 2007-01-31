eb7cd3bb2b67b66ab2e4fb33685753352815bd5b
kajdevos <kajdevos@users.sourceforge.net>
2007-01-31 00:20:12 +0000

    Log stderr in the natural order after stdout

--- system/build-tools/scripts/auto-build/build.sh
+++ system/build-tools/scripts/auto-build/build.sh
@@ -46,12 +46,12 @@ printf "Build started at %s\n" "`date`" > $LOG
 
 # Make sure we have the latest of everything
 echo "Updating Builder"
-build update 2>>$LOG 1>>$LOG
+build update 1>>$LOG 2>>$LOG
 sync
 
 echo "Updating sources"
 cd $SOURCE_DIR/syllable
-cvs -z9 update -dP 2>>$LOG 1>>$LOG
+cvs -z9 update -dP 1>>$LOG 2>>$LOG
 sync
 
 # Copy them
@@ -63,7 +63,7 @@ cd $BUILD_DIR/system
 build prepare system
 
 echo "image system"
-image system 2>>$LOG 1>>$LOG
+image system 1>>$LOG 2>>$LOG
 build log >> $SYSTEM_LOG
 build log failures >> $SYSTEM_FAILURE_LOG
 build log summary >> $SYSTEM_SUMMARY_LOG
@@ -71,7 +71,7 @@ sync
 
 # Build the 'base' profile
 echo "image base"
-image base 2>>$LOG 1>>$LOG
+image base 1>>$LOG 2>>$LOG
 build log > $BASE_LOG
 build log failures > $BASE_FAILURE_LOG
 build log summary > $BASE_SUMMARY_LOG
@@ -79,10 +79,10 @@ sync
 
 # XXXKV: We have to build GCC 4.1.1 with GCC 3.4.3...
 echo "Switching to GCC 3.4.3"
-build install $HOME/Packages/gcc-3.4.3.bin.1.zip 2>>$LOG 1>>$LOG
+build install $HOME/Packages/gcc-3.4.3.bin.1.zip 1>>$LOG 2>>$LOG
 sync
 echo "Building GCC 4.1.1"
-image gcc-libraries 2>>$LOG 1>>$LOG
+image gcc-libraries 1>>$LOG 2>>$LOG
 build log >> $SYSTEM_LOG
 build log failures >> $SYSTEM_FAILURE_LOG
 build log summary >> $SYSTEM_SUMMARY_LOG
@@ -90,33 +90,33 @@ sync
 
 # Grub also must be built with GCC 3.4.3
 echo "Building grub-0.97"
-image grub 2>>$LOG 1>>$LOG
+image grub 1>>$LOG 2>>$LOG
 build log >> $SYSTEM_LOG
 build log failures >> $SYSTEM_FAILURE_LOG
 build log summary >> $SYSTEM_SUMMARY_LOG
 sync
 
 echo "Switching to GCC 4.1.1"
-build install $HOME/Packages/gcc-4.1.1.bin.1.zip 2>>$LOG 1>>$LOG
+build install $HOME/Packages/gcc-4.1.1.bin.1.zip 1>>$LOG 2>>$LOG
 sync
 
 # XXXKV: We have to build Xpdf with Freetype-2.1.10..
 echo "Switching to Freetype-2.1.10"
-build install $HOME/Packages/freetype-2.1.10.bin.1.zip 2>>$LOG 1>>$LOG
+build install $HOME/Packages/freetype-2.1.10.bin.1.zip 1>>$LOG 2>>$LOG
 sync
 echo "Building Xpdf"
-image xpdf 2>>$LOG 1>>$LOG
+image xpdf 1>>$LOG 2>>$LOG
 build log >> $SYSTEM_LOG
 build log failures >> $SYSTEM_FAILURE_LOG
 build log summary >> $SYSTEM_SUMMARY_LOG
 sync
 echo "Switching to Freetype-2.2.1"
-build install $HOME/Packages/freetype-2.2.1.bin.1.zip 2>>$LOG 1>>$LOG
+build install $HOME/Packages/freetype-2.2.1.bin.1.zip 1>>$LOG 2>>$LOG
 sync
 
 # XXXKV: Now on with the show...
 cd stage/image
-build scrub 2>>$LOG 1>>$LOG
+build scrub 1>>$LOG 2>>$LOG
 
 # Mark the end
 echo "Build complete"
