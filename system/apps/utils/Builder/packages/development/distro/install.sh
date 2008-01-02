16c2fa2ed64cde894deb9f54ec4dc24484b288ca
kajdevos <kajdevos@users.sourceforge.net>
2008-01-02 15:48:40 +0000

    Added development files pack

---
+++ system/apps/utils/Builder/packages/development/distro/install.sh
@@ -0,0 +1,59 @@
+#! /bin/sh
+
+if [ "$USER" != "root" ]
+then
+	echo "The installation is not being run by the administrative user."
+	echo "Installation aborted."
+	echo "You can log in as the \"root\" user and try again."
+	exit 1
+fi
+
+echo "This will install the components contained in the developer files pack."
+echo "Previously installed developer files will be deleted first."
+echo ""
+read -p "Do you want to continue (y/N)? " -e answer
+
+if [ "$answer" != "y" ]
+then
+	echo "Installation aborted."
+	exit 1
+fi
+
+echo ""
+
+if [ -e "/system/development" ]
+then
+	for package in `ls /system/development/resources/`
+	do
+		for version in `ls /system/development/resources/$package/`
+		do
+			echo "Unregistering existing /system/development/resources/$package/$version"
+			package unregister /system/development/resources/$package/$version /system/indexes
+			sync
+		done
+	done
+
+	echo "Deleting existing development files"
+	rm -r /system/development
+	sync
+fi
+
+echo ""
+
+echo "Installing files"
+unzip development.zip -d /system
+sync
+
+for package in `ls /system/development/resources/`
+do
+	for version in `ls /system/development/resources/$package/`
+	do
+		echo ""
+		echo "Registering /system/development/resources/$package/$version"
+		package register /system/development/resources/$package/$version /system/indexes
+		sync
+	done
+done
+
+echo ""
+echo "Done."
