d9574e5c272e130f6cbca32583fa3d17fcf1f598
kajdevos <kajdevos@users.sourceforge.net>
2008-01-12 05:40:08 +0000

    Fixed default DHCP

--- system/other/init/network-init.sh
+++ system/other/init/network-init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# This file is generated automatically by the Network Preferences
+ifconfig eth0 down
+echo "Enabling DHCP on interface eth0" >> /var/log/kernel
 dhcpc -d eth0 2>>/var/log/dhcpc
