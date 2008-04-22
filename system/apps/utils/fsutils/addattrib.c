9f8968664ac4010f1261cb62a9a7f391af54d30c
vanders <vanders@liqwyd.com>
2008-04-22 10:05:00 +0000

    make addattrib -f work

--- system/apps/utils/fsutils/addattrib.c
+++ system/apps/utils/fsutils/addattrib.c
@@ -13,12 +13,11 @@ void print_usage( const char* pzName )
 
 int main( int argc, char** argv )
 {
-	int nFile;
-	int i;
-	DIR* pDir;
-	struct dirent* psEntry;
+	int nFile, nDataFile;
+	int i, nPos = 0;
+	char zBuf[1024];
   
-	if ( argc < 4 ) {
+	if ( ( argc < 4 ) || ( argc == 5 && strcasecmp( argv[3], "-f" ) ) || (argc > 5) ) {
 		print_usage( argv[0] );
 		return( 1 );
 	}
@@ -29,8 +28,26 @@ int main( int argc, char** argv )
 		printf( "Failed to open %s: %s\n", argv[1], strerror( errno ) );
 		return( 1 );
 	}
-  
-	write_attr( nFile, argv[2], O_TRUNC, ATTR_TYPE_STRING, argv[3], 0, strlen( argv[3] ) );
+	
+	if( argc == 5 ) {
+		/* Passed -f parameter; write data from file to attribute */
+		nDataFile = open( argv[4], O_RDONLY );
+		if( nDataFile < 0 ) {
+			printf( "Failed to open data file %s: %s\n", argv[4], strerror( errno ) );
+			close( nFile );
+			return( 1 );
+		}
+		do {
+			i = read( nDataFile, zBuf, sizeof( zBuf ) );
+			write_attr( nFile, argv[2], 0, ATTR_TYPE_NONE, zBuf, nPos, i );
+			nPos += i;
+		} while( i == sizeof( zBuf ) );
+	}
+	else
+	{
+		/* Write a string to the attr */
+		write_attr( nFile, argv[2], O_TRUNC, ATTR_TYPE_STRING, argv[3], 0, strlen( argv[3] ) );
+	}
   
 	close( nFile );
 
