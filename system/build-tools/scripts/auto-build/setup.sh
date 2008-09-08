db20b813153e61eb81247489b79d2f10ba631a68
vanders <vanders@liqwyd.com>
2008-09-08 18:53:43 +0000

    Add pkgconfig to the packages list

--- system/build-tools/scripts/auto-build/setup.sh
+++ system/build-tools/scripts/auto-build/setup.sh
@@ -122,6 +122,7 @@ declare -a RESOURCES
 RESOURCES=(	"FLex/flex-2.5.33-3.i586.resource"					\
 			"GNU-AWK/gawk-3.1.6-1.i586.resource"				\
 			"TexInfo/texinfo-4.12-1.i586.resource"				\
+			"PkgConfig/pkg-config-0.22-1.i586.resource"			\
 			"GNU-CompilerCollection/gcc-3.4.3-3.i586.resource"	\
 			"GNU-CompilerCollection/gcc-4.1.2-3.i586.resource" )
 
