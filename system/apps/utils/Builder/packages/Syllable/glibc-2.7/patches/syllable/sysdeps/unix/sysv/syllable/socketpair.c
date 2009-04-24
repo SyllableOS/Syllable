0168141082c9669fc5a55e9a958d5684dc4a4470
vanders <vanders@liqwyd.com>
2009-04-24 22:53:33 +0000

    Include <arpa/inet.h> to get the proper declaration for inet_makeaddr() Fixed segfault where we try to assign a struct to an int during inet_makeaddr() Fix declaration (socketpair() implicitly returns int) Set errno if the requested protocol is not SOCK_STREAM Remove perror() output Tidy up if we exit early

--- system/apps/utils/Builder/packages/Syllable/glibc-2.7/patches/syllable/sysdeps/unix/sysv/syllable/socketpair.c
+++ system/apps/utils/Builder/packages/Syllable/glibc-2.7/patches/syllable/sysdeps/unix/sysv/syllable/socketpair.c
@@ -20,77 +20,78 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <fcntl.h>
-#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
-socketpair( int af, int type, int protocol, int fd[2] )
+int socketpair( int af, int type, int protocol, int fd[2] )
 {
 	int listen_socket;
 	struct sockaddr_in sin[2];
 	int len;
 
 	/* The following is only valid if type == SOCK_STREAM */
-	if( type != SOCK_STREAM )
+	if( type != SOCK_STREAM ) {
+		set_errno( EAFNOSUPPORT );
 		return -1;
+	}
 
 	/* Create a temporary listen socket; temporary, so any port is good */
 	listen_socket = socket( af, type, protocol );
 	if( listen_socket < 0 )
-	{
-		perror( "creating listen_socket" );
 		return -1;
-	}
+
+	memset( &sin[0], 0, sizeof( sin[0] ) );
+	memset( &sin[1], 0, sizeof( sin[1] ) );
+
 	sin[0].sin_family = af;
 	sin[0].sin_port = 0;	/* Use any port number */
-	sin[0].sin_addr.s_addr = inet_makeaddr( INADDR_ANY, 0 );
-	if( bind( listen_socket, (struct sockaddr *)&sin[0], sizeof( sin[0] ) ) < 0 )
-	{
-		perror( "bind" );
+	sin[0].sin_addr = inet_makeaddr( INADDR_ANY, 0 );
+
+	if( bind( listen_socket, (struct sockaddr *)&sin[0], sizeof( sin[0] ) ) < 0 ) {
+		close( listen_socket );
 		return -1;
 	}
 	len = sizeof( sin[0] );
 
 	/* Read the port number we got, so that our client can connect to it */
-	if( getsockname( listen_socket, (struct sockaddr *)&sin[0], &len ) < 0 )
-	{
-		perror( "getsockname" );
+	if( getsockname( listen_socket, (struct sockaddr *)&sin[0], &len ) < 0 ) {
+		close( listen_socket );
 		return -1;
 	}
 
 	/* Put the listen socket in listening mode */
-	if( listen( listen_socket, 5 ) < 0 )
-	{
-		perror( "listen" );
+	if( listen( listen_socket, 5 ) < 0 ) {
+		close( listen_socket );
 		return -1;
 	}
 
 	/* Create the client socket */
 	fd[1] = socket( af, type, protocol );
-	if( fd[1] < 0 )
-	{
-		perror( "creating client_socket" );
+	if( fd[1] < 0 ) {
+		close( listen_socket );
 		return -1;
 	}
 
 	/* Put the client socket in non-blocking connecting mode */
 	fcntl( fd[1], F_SETFL, fcntl( fd[1], F_GETFL, 0 ) | O_NDELAY );
-	if( connect( fd[1], (struct sockaddr *)&sin[0], sizeof( sin[0] ) ) < 0 )
-	{
-		perror( "connect" );
+	if( connect( fd[1], (struct sockaddr *)&sin[0], sizeof( sin[0] ) ) < 0 ) {
+		close( fd[1] );
+		close( listen_socket );
 		return -1;
 	}
 
 	/* At the listen-side, accept the incoming connection we generated */
 	len = sizeof( sin[1] );
-	if( ( fd[0] = accept( listen_socket, (struct sockaddr *)&sin[1], &len ) ) < 0 )
-	{
-		perror( "accept" );
+	if( ( fd[0] = accept( listen_socket, (struct sockaddr *)&sin[1], &len ) ) < 0 ) {
+		close( fd[1] );
+		close( listen_socket );
 		return -1;
 	}
 
 	/* Reset the client socket to blocking mode */
 	fcntl( fd[1], F_SETFL, fcntl( fd[1], F_GETFL, 0 ) & ~O_NDELAY );
-
 	close( listen_socket );
 
 	return 0;
