f225806a7b0c0fab5bee21dab955403ac054a519
kajdevos <kajdevos@users.sourceforge.net>
2009-11-26 02:14:11 +0000

    Fixed syntax

--- system/apps/utils/Builder/packages/Nix-0.13/distro/early-init/nix-paths.sh
+++ system/apps/utils/Builder/packages/Nix-0.13/distro/early-init/nix-paths.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=~/.nix-profile/lib:$LD_LIBRARY_PATH
 while read dir
 do
 	PYTHONPATH=$dir/site-packages:$PYTHONPATH
-done << (find ~/.nix-profile/lib -maxdepth 1 -name python* | sort)
+done < <(find ~/.nix-profile/lib -maxdepth 1 -name python* | sort)
 export PYTHONPATH
 
 # Paths for software compilation
