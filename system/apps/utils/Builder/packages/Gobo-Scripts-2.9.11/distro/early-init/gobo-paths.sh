32438b3c3ef2960800cb5ff799c7297ee713442b
kajdevos <kajdevos@users.sourceforge.net>
2009-12-03 05:25:38 +0000

    Added Gobo tools

---
+++ system/apps/utils/Builder/packages/Gobo-Scripts-2.9.11/distro/early-init/gobo-paths.sh
@@ -0,0 +1,20 @@
+# Run paths
+
+export PATH=$PATH:/System/Links/Executables
+#export DLL_PATH=$DLL_PATH:/System/Links/Libraries
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/System/Links/Libraries
+export MANPATH=$MANPATH:/System/Links/Manuals:/System/Links/Shared/man
+export INFOPATH=$INFOPATH:/System/Links/Manuals/info
+
+while read dir
+do
+	PYTHONPATH=$PYTHONPATH:$dir/site-packages
+done < <(find /System/Links/Libraries -maxdepth 1 -name python* | sort --reverse)
+export PYTHONPATH
+
+# Paths for software compilation
+
+export LIBRARY_PATH=$LIBRARY_PATH:/System/Links/Libraries
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/System/Links/Headers
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/System/Links/Headers
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/System/Links/Libraries/pkgconfig:/System/Links/Shared/pkgconfig
