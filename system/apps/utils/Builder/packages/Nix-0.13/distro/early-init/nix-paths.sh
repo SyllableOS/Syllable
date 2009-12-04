b9444643860a1aa69fc0379ca2535e5e263cb11f
kajdevos <kajdevos@users.sourceforge.net>
2009-12-04 21:26:47 +0000

    LD_LIBRARY_PATH is dangerous for the base system

--- system/apps/utils/Builder/packages/Nix-0.13/distro/early-init/nix-paths.sh
+++ system/apps/utils/Builder/packages/Nix-0.13/distro/early-init/nix-paths.sh
@@ -1,21 +1,21 @@
 # Run paths
 
-#export DLL_PATH=~/.nix-profile/lib:$DLL_PATH
-export LD_LIBRARY_PATH=~/.nix-profile/lib:$LD_LIBRARY_PATH
-export MANPATH=~/.nix-profile/man:~/.nix-profile/share/man:$MANPATH
-export INFOPATH=~/.nix-profile/info:$INFOPATH
+#export DLL_PATH=$DLL_PATH:~/.nix-profile/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.nix-profile/lib
+export MANPATH=$MANPATH:~/.nix-profile/man:~/.nix-profile/share/man
+export INFOPATH=$INFOPATH:~/.nix-profile/info
 
 while read dir
 do
-	PYTHONPATH=$dir/site-packages:$PYTHONPATH
-done < <(find ~/.nix-profile/lib -maxdepth 1 -name 'python*' | sort)
+	PYTHONPATH=$PYTHONPATH:$dir/site-packages
+done < <(find ~/.nix-profile/lib -maxdepth 1 -name 'python*' | sort --reverse)
 export PYTHONPATH
 
-export PERL5LIB=~/.nix-profile/lib/site_perl/5.10.0:~/.nix-profile/lib/5.10.0:$PERL5LIB
+export PERL5LIB=$PERL5LIB:~/.nix-profile/lib/site_perl/5.10.0:~/.nix-profile/lib/5.10.0
 
 # Paths for software compilation
 
-export LIBRARY_PATH=~/.nix-profile/lib:$LIBRARY_PATH
-export C_INCLUDE_PATH=~/.nix-profile/include:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=~/.nix-profile/include:$CPLUS_INCLUDE_PATH
-export PKG_CONFIG_PATH=~/.nix-profile/lib/pkgconfig:~/.nix-profile/share/pkgconfig:$PKG_CONFIG_PATH
+export LIBRARY_PATH=$LIBRARY_PATH:~/.nix-profile/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:~/.nix-profile/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:~/.nix-profile/include
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:~/.nix-profile/lib/pkgconfig:~/.nix-profile/share/pkgconfig
