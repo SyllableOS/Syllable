ed3b228a4fb0bcc1a7cd799ba0f33704dce22881
kajdevos <kajdevos@users.sourceforge.net>
2010-02-02 05:48:30 +0000

    Gobo is moving to /System/Index/

--- system/apps/utils/Builder/packages/Gobo-Scripts-2.9.11/distro/early-init/gobo.sh
+++ system/apps/utils/Builder/packages/Gobo-Scripts-2.9.11/distro/early-init/gobo.sh
@@ -1,22 +1,23 @@
 # Run paths
 
-export PATH=/System/Links/Executables:$PATH
-#export DLL_PATH=/System/Links/Libraries:$DLL_PATH
+export PATH=/System/Index/bin:/System/Links/Executables:$PATH
+#export DLL_PATH=/System/Index/lib:/System/Links/Libraries:$DLL_PATH
 # LD_LIBRARY_PATH is modified earlier in /etc/profile
-export MANPATH=/System/Links/Manuals:/System/Links/Shared/man:$MANPATH
-export INFOPATH=/System/Links/Manuals/info:$INFOPATH
+export MANPATH=/System/Index/share/man:/System/Links/Manuals:/System/Links/Shared/man:$MANPATH
+export INFOPATH=/System/Index/share/info:/System/Links/Manuals/info:$INFOPATH
 
-for dir in `find /System/Links/Libraries -maxdepth 1 -name 'python*' | sort`
+for dir in `find /System/Links/Libraries -maxdepth 1 -name 'python*' | sort` \
+	 `find /System/Index/lib -maxdepth 1 -name 'python*' | sort`
 do
 	PYTHONPATH=$dir/site-packages:$PYTHONPATH
 done
 export PYTHONPATH
 
-export PERL5LIB=/System/Links/Libraries/site_perl/5.10.0:/System/Links/Libraries/5.10.0:$PERL5LIB
+export PERL5LIB=/System/Index/lib/site_perl/5.10.0:/System/Index/lib/5.10.0:/System/Links/Libraries/site_perl/5.10.0:/System/Links/Libraries/5.10.0:$PERL5LIB
 
 # Paths for software compilation
 
-export LIBRARY_PATH=/System/Links/Libraries:$LIBRARY_PATH
-export C_INCLUDE_PATH=/System/Links/Headers:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=/System/Links/Headers:$CPLUS_INCLUDE_PATH
-export PKG_CONFIG_PATH=/System/Links/Libraries/pkgconfig:/System/Links/Shared/pkgconfig:$PKG_CONFIG_PATH
+export LIBRARY_PATH=/System/Index/lib:/System/Links/Libraries:$LIBRARY_PATH
+export C_INCLUDE_PATH=/System/Index/include:/System/Links/Headers:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/System/Index/include:/System/Links/Headers:$CPLUS_INCLUDE_PATH
+export PKG_CONFIG_PATH=/System/Index/lib/pkgconfig:/System/Index/share/pkgconfig:/System/Links/Libraries/pkgconfig:/System/Links/Shared/pkgconfig:$PKG_CONFIG_PATH
