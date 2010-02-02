f49c220b32bbd6100b68b0b736baae2b85ad7484
kajdevos <kajdevos@users.sourceforge.net>
2010-02-02 23:42:40 +0000

    Upgraded Cheyenne, UniServe & QuarterMaster to current development versions

---
+++ system/apps/utils/Builder/packages/Cheyenne--QuarterMaster-current/distro/lib/REBOL/QuarterMaster/controllers/hello.r
@@ -0,0 +1,6 @@
+REBOL [
+	title:	"Hello World"
+	type:	'controller
+]
+
+action "world" does [render "You are running the QuarterMaster web framework."]
---
+++ system/apps/utils/Builder/packages/Cheyenne--current/distro/init/Cheyenne.sh
@@ -0,0 +1,6 @@
+# Maximum logging level:
+# -vvvvv
+# Reload worker processes on each request:
+# -w 0
+# Uncomment the following to start the Cheyenne web server:
+#cd /var && rebol -s /resources/indexes/lib/REBOL/Cheyenne/cheyenne.r &
