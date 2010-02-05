f0f8a73926be0388e19f4b9d02ec46a4ba400771
kajdevos <kajdevos@users.sourceforge.net>
2010-02-05 16:26:09 +0000

    Python is too dumb to know where its own main lib is - proverb about pig's behind comes to mind

--- system/apps/utils/Builder/packages/Python-3.1.1/distro/early-init/Python.sh
+++ system/apps/utils/Builder/packages/Python-3.1.1/distro/early-init/Python.sh
@@ -1,7 +1,9 @@
 # Look up modules in other locations
 
+# For main libpython.so:
+export PYTHONPATH=/resources/Python/lib
+
 for dir in `find /resources/indexes/lib -maxdepth 1 -name 'python*' | sort`
 do
 	PYTHONPATH=$dir/site-packages:$PYTHONPATH
 done
-export PYTHONPATH
