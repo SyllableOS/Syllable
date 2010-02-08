b606fc89ebc50bac86732a96652a0440e6ca53e8
kajdevos <kajdevos@users.sourceforge.net>
2010-02-08 01:53:59 +0000

    Fixed Cheyenne startup

--- system/apps/utils/Builder/packages/Cheyenne--current/distro/init/Cheyenne.sh
+++ system/apps/utils/Builder/packages/Cheyenne--current/distro/init/Cheyenne.sh
@@ -1,6 +1,9 @@
+# Make the logs go in the standard system place:
+cd /var
+
 # Maximum logging level:
 # -vvvvv
 # Reload worker processes on each request:
 # -w 0
 # Uncomment the following to start the Cheyenne web server:
-#cd /var && rebol -s /resources/indexes/lib/REBOL/Cheyenne/cheyenne.r &
+#rebol-core -s /resources/indexes/lib/REBOL/Cheyenne/cheyenne.r &
