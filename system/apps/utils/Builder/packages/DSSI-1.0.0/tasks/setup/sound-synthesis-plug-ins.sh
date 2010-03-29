20ab01ca1dd51017da279a59d0b8e955bb74d426
kajdevos <kajdevos@users.sourceforge.net>
2010-03-29 23:49:03 +0000

    Added audio packages, from Stefano

---
+++ system/apps/utils/Builder/packages/DSSI-1.0.0/tasks/setup/sound-synthesis-plug-ins.sh
@@ -0,0 +1,1 @@
+export DSSI_PATH=~/extensions/dssi:/system/extensions/dssi:/resources/index/framework/libraries/dssi:/resources/indexes/lib/dssi:/usr/local/lib/dssi
---
+++ system/apps/utils/Builder/packages/LADSPA-1.13/tasks/setup/sound-effect-plug-ins.sh
@@ -0,0 +1,1 @@
+export LADSPA_PATH=~/extensions/ladspa:/system/extensions/ladspa:/resources/index/framework/libraries/ladspa:/resources/indexes/lib/ladspa:/usr/local/lib/ladspa
