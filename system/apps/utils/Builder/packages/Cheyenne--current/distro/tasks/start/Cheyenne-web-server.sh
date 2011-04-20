6c6b12327e585c9d43d94f83334c5aa6e73c5cf5
kajdevos <kajdevos@users.sourceforge.net>
2011-04-20 00:40:43 +0000

    Upgraded Cheyenne, to binary version

--- system/apps/utils/Builder/packages/Cheyenne--current/distro/tasks/start/Cheyenne-web-server.sh
+++ system/apps/utils/Builder/packages/Cheyenne--current/distro/tasks/start/Cheyenne-web-server.sh
@@ -1,9 +1,15 @@
-# Make the logs go in the standard system place:
-cd /var
-
 # Maximum logging level:
-# -vvvvv
+# -vvvvvv
 # Reload worker processes on each request:
 # -w 0
+
 # Uncomment the following to start the Cheyenne web server:
+
+# Binary version:
+#cd /resources/Cheyenne/framework/REBOL/Cheyenne
+#cheyenne &
+
+# Source version:
+# Make the logs go in the standard system place:
+#cd /var
 #r2-core -s /resources/index/framework/REBOL/Cheyenne/cheyenne.r &
