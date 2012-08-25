661141a8d21d879e065f552489ed4bbb1f03f40b
kajdevos <kajdevos@users.sourceforge.net>
2012-08-25 23:43:47 +0000

    Added incomplete Mongrel

---
+++ system/apps/utils/Builder/packages/Mongrel--current/distro/tasks/start/Mongrel-web-server.sh
@@ -0,0 +1,5 @@
+# Uncomment the following to start the Mongrel web server:
+#m2sh load -config mysite.conf
+#m2sh servers -db tests/config.sqlite
+#m2sh start -db tests/config.sqlite -host localhost &
+#mongrel2 tests/config.sqlite server_uuid
