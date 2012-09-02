48389138e8776972ada94684b79a15d9f1bb5ecf
kajdevos <kajdevos@users.sourceforge.net>
2012-09-02 21:12:59 +0000

    Added Mongrel configuration, documentation

--- system/apps/utils/Builder/packages/Mongrel--current/distro/tasks/start/Mongrel-web-server.sh
+++ system/apps/utils/Builder/packages/Mongrel--current/distro/tasks/start/Mongrel-web-server.sh
@@ -1,5 +1,4 @@
 # Uncomment the following to start the Mongrel web server:
-#m2sh load -config mysite.conf
-#m2sh servers -db tests/config.sqlite
-#m2sh start -db tests/config.sqlite -host localhost &
-#mongrel2 tests/config.sqlite server_uuid
+#cd /resources/Mongrel/state
+##m2sh load
+#m2sh start -name Mongrel
